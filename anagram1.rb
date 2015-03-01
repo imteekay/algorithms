9b8d98650dff4c847318914cca842c7c2bd18278
Leandro Tk <leandro.kinoshita@codus.com>
2015-03-01 13:32:50 -0300

    Improved is_anagram function in first anagram

--- anagram1.rb
+++ anagram1.rb
@@ -1,31 +1,37 @@
 def is_anagram?(first_word, second_word)
-	letters_list = second_word.split('')
+	if first_word.length == second_word.length
+		letters_list = second_word.split('')
 
-	position1 = 0
-	still_ok = true
+		position1 = 0
+		still_ok = true
 
-	while position1 < first_word.length && still_ok
-		position2 = 0
-		found = false
-		while position2 < letters_list.length && !found
-			if first_word[position1] == letters_list[position2]
-				found = true
-			else
-				position2 += 1
+		while position1 < first_word.length && still_ok
+			position2 = 0
+			found = false
+			while position2 < letters_list.length && !found
+				if first_word[position1] == letters_list[position2]
+					found = true
+				else
+					position2 += 1
+				end
 			end
+
+			if found
+				letters_list[position2] = nil
+			else
+				still_ok = false
+			end
+
+			position1 += 1
 		end
 
-		if found
-			letters_list[position2] = nil
-		else
-			still_ok = false
-		end
-
-		position1 += 1
+		still_ok
+	else
+		false
 	end
-
-	still_ok
 end
 
+p is_anagram?('abcde', 'edcbaa')
+p is_anagram?('abcde', 'edcba')
 p is_anagram?("nada", "adan")
 p is_anagram?("oi", "tchau")
