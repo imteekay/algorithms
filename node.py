5e7160f0a89c108a459ed076aeafefd335b7f651
Leandro Tk <leandro.kinoshita@codus.com>
2015-04-05 20:57:37 -0300

    Implemented node class

---
+++ node.py
@@ -0,0 +1,17 @@
+class Node:
+
+	def __init__(self, initial_data):
+		self.data = initial_data
+		self.next = None
+
+	def get_data(self):
+		return self.data
+
+	def set_data(self, new_data):
+		self.data = new_data
+
+	def get_next(self):
+		return self.next
+
+	def set_next(self, new_next):
+		self.next = new_next
