a39550cfca8c27749cc1af71ed2149acb963aa45
Leandro Tk <leandrodtk@hotmail.com>
2015-08-16 15:44:11 -0300

    Implemented code to concurso problem

---
+++ programming_contests/acm_icpc_br/concurso.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+	int n, l, c, result;
+	string conto;
+
+	while (scanf("%d %d %d", &n, &l, &c) != EOF) {
+		cin.ignore();
+		getline(cin, conto);		
+
+		if (conto.size() == (l * c))
+			result = 1;
+		else
+			result = (conto.size() / (l * c)) + 1;
+
+		cout << result << endl;
+	}
+
+	return 0;
+}
---
+++ programming_contests/acm_icpc_br/concurso.in
@@ -0,0 +1,8 @@
+14 4 20
+Se mana Piedade tem casado com Quincas Borba apenas me daria uma esperanca colateral
+16 3 30
+No dia seguinte entrou a dizer de mim nomes feios e acabou alcunhando me Dom Casmurro
+5 2 2
+a de i de o
+5 2 2
+a e i o u
