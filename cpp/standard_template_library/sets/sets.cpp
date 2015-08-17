278ac1163598b4dbd6564d2c4b67e0e355804346
Leandro Tk <leandro.kinoshita@codus.com.br>
2015-08-17 17:03:10 -0300

    Removing duplicates and sorting the vector elements using set

--- cpp/standard_template_library/sets/sets.cpp
+++ cpp/standard_template_library/sets/sets.cpp
@@ -4,6 +4,7 @@
 // check whether elements are present in set
 
 #include <iostream>
+#include <vector>
 #include <set>
 
 using namespace std;
@@ -28,6 +29,31 @@ int main() {
 
   // need to be 50
   cout << s.size() << endl;
+
+  // calculate the sum of set elements
+  int total = 0;
+  for (set<int>::const_iterator iterator = s.begin(); iterator != s.end(); iterator++)
+    total += *iterator;
+
+  cout << total << endl;
+
+  // removing the duplicates from a vector
+  vector<int> v;
+  for (int i = 10; i > 0; i--)
+    v.push_back(i);
+
+  v.push_back(5);
+
+  set<int> s2;
+  for (int i = 1; i <= 10; i++)
+    s2.insert(v[i]);
+
+  vector<int> v2;
+  for (set<int>::const_iterator iterator = s2.begin(); iterator != s2.end(); iterator++)
+    v2.push_back(*iterator);
+
+  for (int i = 0; i < 10; i++)
+    cout << v2[i] << endl;
 
   return 0;
 }
