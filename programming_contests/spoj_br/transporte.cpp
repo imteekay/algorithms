8b430326d57f9e13e34b7f231d5b85d3656fea96
Leandro Tk <leandrodtk@hotmail.com>
2015-08-17 23:16:02 -0300

    Added transporte solution for SPOJ

---
+++ programming_contests/spoj_br/transporte.cpp
@@ -0,0 +1,19 @@
+// TRANSP11
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a, b, c, x, y, z;
+	
+	cin >> a >> b >> c >> x >> y >> z;
+	
+	if ((a > x) || (b > y) || (c > z))
+		cout << 0;
+	else {
+		cout << ((x/a) * (y/b) * (z/c));
+	}
+	
+	return 0;
+}
