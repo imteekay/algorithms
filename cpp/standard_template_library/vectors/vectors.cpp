750b913d0a80d19e2be3decbb355e400455c3116
Leandro Tk <leandro.kinoshita@codus.com.br>
2015-08-18 15:15:13 -0300

    Improved undertand about cpp vector

--- cpp/standard_template_library/vectors/vectors.cpp
+++ cpp/standard_template_library/vectors/vectors.cpp
@@ -16,5 +16,18 @@ int main() {
 	for (int i = 0; i < v.size(); i++)
 		cout << v[i] << endl;
 
+  cout << v.size() << endl;
+
+  // remove last element
+  v.pop_back();
+
+  // remove first element
+  v.erase(v.begin());
+
+  for (int i = 0; i < v.size(); i++)
+    cout << v[i] << endl;
+
+  cout << v.size() << endl;
+
 	return 0;
 }
