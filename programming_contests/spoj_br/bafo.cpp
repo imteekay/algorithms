6459634de9e3bcdebe6f59c1477e6f98c3a62e1c
Leandro Tk <leandrodtk@hotmail.com>
2015-08-17 23:16:42 -0300

    Added bafo solution for SPOJ

---
+++ programming_contests/spoj_br/bafo.cpp
@@ -0,0 +1,38 @@
+// BAFO
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+  int n, t_beto=0, t_aldo=0, x, y, counter=1;
+  string result;
+
+  cin >> n;
+
+  if(n == 0)
+    return 0;
+
+  while(n != 0) {
+
+    for (int i = 0; i < n; i++) {
+      cin >> x >> y;
+      t_aldo += x;
+      t_beto += y;      
+    }
+
+    if (t_aldo > t_beto)
+      result = "Aldo";
+    else
+      result = "Beto";
+
+    cout << "Teste " << counter << endl;
+    cout << result << endl << endl;
+
+    cin >> n;
+    counter++;
+    t_beto = 0;
+    t_aldo = 0;
+  }
+}
