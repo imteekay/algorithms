14a6f23f1dd7b6076c92861c4b7e47a343f9c429
Leandro Tk <leandro.kinoshita@codus.com.br>
2015-08-19 10:36:22 -0300

    Improved fibonaci function in a DP way

--- algorithms/dynamic_programming/dynamic_programming.cpp
+++ algorithms/dynamic_programming/dynamic_programming.cpp
@@ -14,7 +14,8 @@ int fibonacci_dp(int n) {
     if (n <= 1) return 1;
 
     std::vector<int> memo(n + 1);
-    memo[0] = memo[1] = 1;
+    memo[0] = 0;
+    memo[1] = memo[2] = 1;
 
     for (int i = 2; i <= n; ++i)
         memo[i] = memo[i-1] + memo[i-2];
@@ -24,13 +25,13 @@ int fibonacci_dp(int n) {
 
 int main() {
 
-  int n = 5;
+  int n = 5, m = 5;
 
   // recursive algorithm without DP
   cout << fibonacci(n) << endl;
 
   // memoized DP algorithm: add in the map. If we have the element in the map, return the result
-  cout << fibonacci_dp(n) << endl;
+  cout << fibonacci_dp(m) << endl;
 
   return 0;
 }
