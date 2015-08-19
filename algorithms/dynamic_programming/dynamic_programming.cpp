b45cc1e6cdea44c7a5ebf038cd78b0a69d7d7890
Leandro Tk <leandro.kinoshita@codus.com.br>
2015-08-19 11:14:17 -0300

    Added description for DP

--- algorithms/dynamic_programming/dynamic_programming.cpp
+++ algorithms/dynamic_programming/dynamic_programming.cpp
@@ -4,24 +4,32 @@
 using namespace std;
 
 int fibonacci(int n) {
-  // exponation time - T(n) = T(n - 1) + T(n - 2) + const(1)
+  // exponation time - T(n) = T(n - 1) + T(n - 2) + const(1) --> O(2^(n/2))
   if (n <= 2) return 1;
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int fibonacci_dp(int n) {
-    if (n == 0) return 0;
-    else if (n == 1) return 1;
+  // memoized calls cost is n: T(n) --> O(n)
+  // DP == recursion + memoization
+  // memoize (remember):
+  // - remember & reuse solutions
+  // - know the sub-problems that help solve the problem
+  // time = #subproblems * (time / subproblems)
+  //        (    n     )   (     constant     )
 
-    std::vector<int> memo(n + 1);
-    memo[0] = 0;
-    memo[1] = memo[2] = 1;
+  if (n == 0) return 0;
+  else if (n == 1) return 1;
 
-    for (int i = 2; i <= n; ++i)
-        memo[i] = memo[i-1] + memo[i-2];
+  std::vector<int> memo(n + 1);
+  memo[0] = 0;
+  memo[1] = memo[2] = 1;
 
-    return memo.back();
+  for (int i = 2; i <= n; ++i)
+      memo[i] = memo[i-1] + memo[i-2];
+
+  return memo.back();
 }
 
 int main() {
