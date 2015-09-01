3b20d237c6e2de3678965aa6970c6f36c7c7cfd6
Leandro Tk <leandrodtk@hotmail.com>
2015-08-31 22:49:02 -0300

    Implemented prime number verification function

---
+++ algorithms/prime_number/prime_number.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int verifica_primo(int n) {
+	
+  int total_divisivel = 0;
+
+	if (n == 2)
+		return 1;
+	
+	if ((n % 2) == 0)
+		return 0;
+
+	int s = sqrt(n);
+
+	for (int i = 3; i <= s; i += 2) {
+		if (n % i == 0)
+			return 0;
+	}
+
+	return 1;
+}
+
+int main() {
+    
+
+  int n;
+  cin >> n;
+
+  while (n) {
+    if (verifica_primo(n))
+      cout << "True" << endl;
+    else
+      cout << "False" << endl;
+
+    cin >> n;
+  }
+
+  return 0;
+}
