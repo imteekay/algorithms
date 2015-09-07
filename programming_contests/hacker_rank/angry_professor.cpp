8010e5f9ed1cc35fd28682fd28d8f599191b6cf9
Leandro Tk <leandrodtk@hotmail.com>
2015-09-07 13:42:54 -0300

    Implemented algorithm for angry professor problem

---
+++ programming_contests/hacker_rank/angry_professor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int t, n, k, time, total_students_arrived_before_class=0;
+
+	cin >> t;
+
+	while (t--) {
+		cin >> n >> k;
+
+		for (int i = 0; i < n; i++) {
+			cin >> time;			
+			if (time <= 0) total_students_arrived_before_class++;
+		}
+
+		if (total_students_arrived_before_class >= k) cout << "NO" << endl;
+		else cout << "YES" << endl;
+		total_students_arrived_before_class=0;
+	}
+
+	return 0;
+}
