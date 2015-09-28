015414766aa2fe40fd60fd95ab8269b3f2bf7a33
Leandro Tk <leandrodtk@hotmail.com>
2015-09-28 02:50:56 -0300

    Implemented algorithm for loop musical problem

---
+++ programming_contests/spoj_br/bapostas.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int n, num, total=0;
+
+	cin >> n;
+
+	while (n) {
+
+		for (i = 0; i < n; i++) {
+			cin >> num;
+		}
+
+		cin >> n;
+	}
+
+	return 0;
+}
