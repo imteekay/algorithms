c7327d66ff565517d0098644cf6379bb0598b4cc
Leandro Tk <leandrodtk@hotmail.com>
2015-09-28 23:50:17 -0300

    Implemented algorithm for sorvete problem

--- programming_contests/spoj_br/sorvete.cpp
+++ programming_contests/spoj_br/sorvete.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
 	
 	int test=1, s, p;
+	vector< pair<int, int> > vetorzin;
 	vector< pair<int, int> > v;
 
 	cin >> p >> s;
 
 	while (s + p != 0) {
 
-		int n1, n2;
-		cin >> n1 >> n2;
-		v.push_back(make_pair(n1, n2));
-		s--;
-
 		for (int i = 0; i < s; i++) {
+			int n1, n2;
 			cin >> n1 >> n2;
+			vetorzin.push_back(make_pair(n1, n2));			
+		}
 
-			if (v.back().second < n1) v.push_back(make_pair(n1, n2));
-			else if (v.back().second < n2) v.back().second = n2;			
+		sort(vetorzin.begin(), vetorzin.end());
+
+		v.push_back(make_pair(vetorzin[0].first, vetorzin[0].second));
+
+		for (int i = 1; i < s; i++) {
+			if (v.back().second < vetorzin[i].first) v.push_back(make_pair(vetorzin[i].first, vetorzin[i].second));
+			else if (v.back().second < vetorzin[i].second) v.back().second = vetorzin[i].second;
 		}
 
 		cout << "Teste " << test << endl;
@@ -32,6 +37,7 @@ int main() {
 
 		cout << endl;
 		v.clear();
+		vetorzin.clear();
 		test++;
 		cin >> p >> s;
 	}
