a4d22a349f595826f92f43a69051377ba7f61f04
Leandro Tk <leandro.kinoshita@codus.com.br>
2015-09-30 13:44:21 -0300

    Implemented algorithm for aero problem

--- programming_contests/spoj_br/aero.cpp
+++ programming_contests/spoj_br/aero.cpp
@@ -3,15 +3,11 @@
 #include <algorithm>
 #include <map>
 
-map<int, int> find_by_value(map<int, int> mapper) {
-  for (it = mapper.begin(); it != mapper.end(); ++it)
-    if (it->second == )
-      return it->first;
-}
+using namespace std;
 
 int main() {
 
-  int n1, n2;
+  int n1, n2, test=1;
 
   cin >> n1 >> n2;
 
@@ -20,17 +16,40 @@ int main() {
     vector< pair<int, int> > v;
 
     while (n2--) {
-      v.push_back(make_pair(n1, n2));
+      int line1, line2;
+      cin >> line1 >> line2;
+      v.push_back(make_pair(line1, line2));
     }
 
     map<int, int> mapper;
 
     for (int i = 0; i < v.size(); i++) {
       if (mapper.find(v[i].first) != mapper.end()) mapper.find(v[i].first)->second++;
-      else if (mapper.find(v[i].second) != mapper.end()) mapper.find(v[i].second)->second++;
-      else mapper[v[i]] = 1;
+      else mapper[v[i].first] = 1;
+
+      if (mapper.find(v[i].second) != mapper.end()) mapper.find(v[i].second)->second++;
+      else mapper[v[i].second] = 1;
     }
 
+    vector<int> lines;
+
+    for (map<int, int>::iterator it = mapper.begin(); it != mapper.end(); ++it) {
+      lines.push_back(it->second);
+    }
+
+    int max=0;
+
+    for (int i = 0; i < lines.size(); i++)
+      if (lines[i] > max) max = lines[i];
+
+    cout << "Teste " << test << endl;
+
+    for (int i = 0; i < lines.size(); i++)
+      if (lines[i] == max) printf("%d ", i+1);
+
+    printf("\n\n");
+
+    test++;
     cin >> n1 >> n2;
   }
 
