b81e23c4409aa637388938c98a4d7bc8153640e4
Leandro Tk <leandrodtk@hotmail.com>
2015-10-05 23:42:50 -0300

    Improved knapsack algorithm to be clear

--- algorithms/knapsack/knapsack1.cpp
+++ algorithms/knapsack/knapsack1.cpp
@@ -4,28 +4,27 @@ int max(int a, int b) {
   return (a > b)? a : b; 
 }
  
-int knapsack(int W, int wt[], int val[], int n) {
-  int i, w;
-  int K[n+1][W+1];
+int knapsack(int totalWeight, int weights[], int values[], int n) {  
+  int K[n+1][totalWeight+1];
  
-  for (i = 0; i <= n; i++) {
-    for (w = 0; w <= W; w++) {
-      if (i==0 || w==0) K[i][w] = 0;
-      else if (wt[i-1] <= w) K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
-      else K[i][w] = K[i-1][w];
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= totalWeight; j++) {
+      if (i == 0 || j == 0) K[i][j] = 0;
+      else if (weights[i-1] <= j) K[i][j] = max(values[i-1] + K[i-1][j - weights[i-1]],  K[i-1][j]);
+      else K[i][j] = K[i-1][j];
     }
   }
  
-  return K[n][W];
+  return K[n][totalWeight];
 }
  
 int main() {  
-  int val[] = {4, 1, 2, 5};
-  int wt[] = {100, 100, 120, 80};
-  int W = 200;
-  int n = sizeof(val)/sizeof(val[0]);
+  int values[] = { 4, 1, 2, 5 };
+  int weighs[] = { 100, 100, 120, 80 };
+  int totalWeight = 200;
+  int n = sizeof(values) / sizeof(values[0]);
   
-  printf("%d", knapsack(W, wt, val, n));
+  printf("%d\n", knapsack(totalWeight, weighs, values, n));
   
   return 0;
 }
