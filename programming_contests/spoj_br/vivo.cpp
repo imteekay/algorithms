870efa25733bd6b77648eb4e92d5d2a5be030cbb
Leandro Tk <leandrodtk@hotmail.com>
2015-10-07 00:41:24 -0300

    Improved vivo algorithm

--- programming_contests/spoj_br/vivo.cpp
+++ programming_contests/spoj_br/vivo.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 
 	int n1, n2, test=1;
-	cin >> n1 >> n2;
+	scanf("%d %d", &n1, &n2);
 
 	while (n1 + n2 != 0) {
 
@@ -14,33 +14,30 @@ int main() {
 		vector<int> v;
 
 		for (int i = 0; i < n1; i++) {
-			cin >> temp;
+			scanf("%d", &temp);
 			v.push_back(temp);
 		}
 
-		while (1) {
+		while (n2--) {
 			int n, j, erasing=0;
-			cin >> n;
-			cin >> j;
+			scanf("%d %d", &n, &j);
 
 			for (int i = 0; i < n; i++) {
 				int t;
-				cin >> t;
+				scanf("%d", &t);
 				
 				if (t != j) {
 					v.erase(v.begin() + i - erasing);
 					erasing++;
 				}
 			}
-
-			if (v.size() == 1) break;
 		}
 
 		cout << "Teste " << test << endl;
-		cout << v[0] << endl;
+		cout << v[0] << endl << endl;		
 
 		test++;
-		cin >> n1 >> n2;
+		scanf("%d %d", &n1, &n2);
 	}
 
 	return 0;
