19b05d1586fe505d544d4d20bd48e4f0bd059f34
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-01-20 13:41:34 -0200

    Implemented area of a circle problem

---
+++ programming_contests/uri_online_judge/area_of_circle.cpp
@@ -0,0 +1,18 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1002
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+  const double PI = 3.1415926;
+  double r;
+  cin >> r;
+
+  cout << fixed << setprecision(4) << "A=" << PI * pow(r, 2) << endl;
+
+  return 0;
+}
