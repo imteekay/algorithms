8f59b138a3803d547418cb4e2e48e2e27ade3a17
Leandro Tk <leandrodtk@hotmail.com>
2016-01-20 00:28:04 -0200

    Implemented algorithm for extremely easy problem on uri online judge

---
+++ programming_contests/uri_online_judge/extremely_basic.cpp
@@ -0,0 +1,13 @@
+// https://www.urionlinejudge.com.br/judge/en/runs/add/1001
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+	int a, b, x;
+	cin >> a >> b;
+	cout << "X = " << a + b << endl;
+
+	return 0;
+}
