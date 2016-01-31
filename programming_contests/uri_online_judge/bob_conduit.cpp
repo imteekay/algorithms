ca7468acdb1f75e91f6520bcada367ee590bd058
Leandro Tk <leandrodtk@hotmail.com>
2016-01-31 11:57:40 -0200

    Solved bob conduit problem

---
+++ programming_contests/uri_online_judge/bob_conduit.cpp
@@ -0,0 +1,23 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1589
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	long long int n1, n2;
+
+	cin >> n;
+
+	while (n--) {
+
+		cin >> n1 >> n2;
+
+		cout << n1 + n2 << endl;
+
+	}
+
+	return 0;
+}
