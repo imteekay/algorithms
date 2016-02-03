bd568422fd83b4fd6e6468878bdaa4123d1dd20d
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-02-03 17:34:40 -0200

    Solved eletrcial outlet problem

---
+++ programming_contests/uri_online_judge/eletrical_outlet.cpp
@@ -0,0 +1,16 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1930
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int t1, t2, t3, t4;
+
+  cin >> t1 >> t2 >> t3 >> t4;
+
+  cout << t1 + t2 + t3 + t4 - 3 << endl;
+
+  return 0;
+}
