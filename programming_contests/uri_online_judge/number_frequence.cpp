e1c2a86dbf5241b485d5593b4413be314525eb99
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-02-05 10:28:07 -0200

    solved some problems

---
+++ programming_contests/uri_online_judge/number_frequence.cpp
@@ -0,0 +1,24 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1171
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+  int n, x;
+  cin >> n;
+
+  vector<int> v1;
+  vector<int> v2;
+
+  for (int i = 0; i < n; i++) {
+    cin >> x;
+
+    v1.push_back(x);
+
+  }
+
+  return 0;
+}
