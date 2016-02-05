45369872234367fae74084275f08364908b360b7
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-02-05 12:57:33 -0200

    started to implement number frquence problem

--- programming_contests/uri_online_judge/number_frequence.cpp
+++ programming_contests/uri_online_judge/number_frequence.cpp
@@ -7,17 +7,29 @@ using namespace std;
 
 int main() {
 
-  int n, x;
+  int n, x, found=0;
   cin >> n;
 
   vector<int> v1;
   vector<int> v2;
 
   for (int i = 0; i < n; i++) {
+    v2.push_back(0);
+  }
+
+  for (int i = 0; i < n; i++) {
     cin >> x;
 
-    v1.push_back(x);
+    for (int i = 0; i < v1.size(); i++) {
+      if (v1[i] == x) {
+        found = 1;
+        break;
+      }
+    }
 
+    if (!found) v1.push_back(x);
+
+    found = 0;
   }
 
   return 0;
