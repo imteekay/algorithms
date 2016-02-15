4e3f420fa75650649849ab7ec09bd2284474d94c
Leandro Tk <leandrodtk@hotmail.com>
2016-02-15 08:59:31 -0200

    Started to code flavious joseph problem

---
+++ programming_contests/uri_online_judge/flavious_josephus_legend.cpp
@@ -0,0 +1,21 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1030
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int nc, n, k;
+
+	cin >> nc;
+
+	while (nc--) {
+		cin >> n >> k;
+
+		
+
+	}
+
+	return 0;
+}
