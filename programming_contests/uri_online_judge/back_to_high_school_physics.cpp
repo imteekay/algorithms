60dd2883010cce888f1a3396e77c7afd092d8fb9
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-02-25 18:27:02 -0300

    Solved Back to High School Physics problem

---
+++ programming_contests/uri_online_judge/back_to_high_school_physics.cpp
@@ -0,0 +1,14 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1197
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int n1, n2;
+
+  while (scanf("%d %d", &n1, &n2) != EOF) cout << n1 * n2 * 2 << endl;
+
+  return 0;
+}
