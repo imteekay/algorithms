73e8ca9da0f9f8f9cf1be6e71cf3db4750d712b4
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-02-25 16:33:47 -0300

    Implemented algorithm to solve coast guard problem

---
+++ programming_contests/uri_online_judge/coast_guard.cpp
@@ -0,0 +1,22 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1247
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+  float d, f, g, n, distancia_perpendicular = 12;
+
+  while(scanf("%f %f %f", &d, &f, &g) != EOF) {
+    n = sqrt((144 + d*d));
+
+    if ((12 / f) < (n / g))
+      cout << 'N' << endl;
+    else
+      cout << 'S' << endl;
+  }
+
+  return 0;
+}
