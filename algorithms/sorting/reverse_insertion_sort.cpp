214a649d60897c36d04e55bd052bfaf369baf05d
Leandro Tk <leandrodtk@hotmail.com>
2016-02-26 02:05:21 -0300

    Implemented reverse insetion sort

--- algorithms/sorting/reverse_insertion_sort.cpp
+++ algorithms/sorting/reverse_insertion_sort.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-void insertion_sort(int n, int array[]) {
+void reverse_insertion_sort(int n, int array[]) {
 	int x, j;
 	for (int i = 1; i < n; i++) {
 		x = array[i];		
-		for (j = i - 1; j >= 0 && array[j] > x; j--) array[j+1] = array[j];
+		for (j = i - 1; j >= 0 && array[j] < x; j--) array[j+1] = array[j];
 		array[j+1] = x;
 	}
 }
@@ -15,7 +15,7 @@ int main() {
 
 	int array[] = { 17, 3, 2, 10 };
 
-	insertion_sort(4, array);
+	reverse_insertion_sort(4, array);
 
 	for (int i = 0; i < 4; i++) cout << array[i] << " ";
 
