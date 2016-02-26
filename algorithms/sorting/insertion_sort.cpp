56d69c9caf4704e77eb4f992de695bebbdd70f8d
Leandro Tk <leandrodtk@hotmail.com>
2016-02-26 02:02:01 -0300

    Implemented insertion sort algorithm

---
+++ algorithms/sorting/insertion_sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void insertion_sort(int n, int array[]) {
+	int x, j;
+	for (int i = 1; i < n; i++) {
+		x = array[i];		
+		for (j = i - 1; j > 0 && array[j] > x; j--) array[j+1] = array[j];
+		array[j+1] = x;
+	}
+}
+
+int main() {
+
+	int array = { 17, 3, 2, 10 };
+
+	insertion_sort(4, array);
+
+	for (int i = 0; i < 4; i++) cout << array[i] << " ";
+
+	return 0;
+}
