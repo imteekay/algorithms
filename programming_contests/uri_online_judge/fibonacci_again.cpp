68c05a1fe9be54e9c68096bcfffede1fbaa923c7
Leandro Tk <leandrodtk@hotmail.com>
2016-02-29 00:33:49 -0300

    Trying to improve fibonacci again algorithm

--- programming_contests/uri_online_judge/fibonacci_again.cpp
+++ programming_contests/uri_online_judge/fibonacci_again.cpp
@@ -1,24 +1,49 @@
 // https://www.urionlinejudge.com.br/judge/en/problems/view/1531
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int fibonacci_dp(int n) {
-  if (n == 0) return 0;
-  else if (n == 1) return 1;
-  vector<int> memo(n + 1);
-  memo[0] = 0;
-  memo[1] = memo[2] = 1;
-  for (int i = 2; i <= n; ++i) memo[i] = memo[i-1] + memo[i-2];
-  return memo.back();
+#define MOD 1000000007;
+long long int a,b,c,d;
+
+void fast_fib(long long int n,long long int ans[]) {
+    if (n == 0) {
+      ans[0] = 0;
+      ans[1] = 1;
+      return;
+    }
+
+    fast_fib((n/2),ans);
+    a = ans[0];
+    b = ans[1];
+    c = 2 * b - a;
+    
+    if(c < 0) c += MOD;
+    
+    c = (a * c) % MOD;
+    d = (a*a + b*b) % MOD;
+
+    if (n % 2 == 0) {
+      ans[0] = c;
+      ans[1] = d;
+    } else {
+      ans[0] = d;
+      ans[1] = c+d;
+    }
 }
 
 int main() {
-	long int n1, n2;
+	long long int n1;
+	int n2;	
 	
-	while (scanf("%li %li", &n1, &n2) != EOF) {
-		cout << fibonacci_dp(fibonacci_dp(n1)) % n2 << endl;
+	while (scanf("%lli %i", &n1, &n2) != EOF) {
+		long long int ans[2]={0};
+    fast_fib(n1, ans);
+    long long int res[2]={0};
+    fast_fib(ans[0], res);
+		cout << res[0] % n2 << endl;
 	}
 	
 	return 0;
