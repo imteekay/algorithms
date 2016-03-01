ef2adff98d4f3f342860bcdd9a632f7bf620080d
Leandro Tk <leandrodtk@hotmail.com>
2016-02-29 22:11:23 -0300

    The indentation was crazing me >.<

--- programming_contests/uri_online_judge/fibonacci_again.cpp
+++ programming_contests/uri_online_judge/fibonacci_again.cpp
@@ -9,29 +9,29 @@ using namespace std;
 long long int a,b,c,d;
 
 void fast_fib(long long int n,long long int ans[]) {
-    if (n == 0) {
-      ans[0] = 0;
-      ans[1] = 1;
-      return;
-    }
+  if (n == 0) {
+    ans[0] = 0;
+    ans[1] = 1;
+    return;
+  }
 
-    fast_fib((n/2),ans);
-    a = ans[0];
-    b = ans[1];
-    c = 2 * b - a;
-    
-    if(c < 0) c += MOD;
-    
-    c = (a * c) % MOD;
-    d = (a*a + b*b) % MOD;
+  fast_fib((n/2),ans);
+  a = ans[0];
+  b = ans[1];
+  c = 2 * b - a;
+  
+  if (c < 0) c += MOD;
+  
+  c = (a * c) % MOD;
+  d = (a*a + b*b) % MOD;
 
-    if (n % 2 == 0) {
-      ans[0] = c;
-      ans[1] = d;
-    } else {
-      ans[0] = d;
-      ans[1] = c+d;
-    }
+  if (n % 2 == 0) {
+    ans[0] = c;
+    ans[1] = d;
+  } else {
+    ans[0] = d;
+    ans[1] = c+d;
+  }
 }
 
 int main() {
