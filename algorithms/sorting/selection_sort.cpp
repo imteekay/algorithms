5ee0eb262258b7ba8232937bd6c972b4a96661f8
Leandro Tk <leandrodtk@hotmail.com>
2016-03-04 22:36:48 -0300

    Implemented selection sort

--- algorithms/sorting/selection_sort.cpp
+++ algorithms/sorting/selection_sort.cpp
@@ -1,9 +1,26 @@
-#define swap(a, b) { int x=a; a=b; b=x  }
+#include <iostream>
+using namespace std;
+
+#define swap(a, b) { int x=a; a=b; b=x; }
 
 void selection_sort(int v[], int n) {
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n - i; j++) {
-      if (v[j] > v[j+1]) troca(v[j], v[j+1]);
-    }
+  for (int i = 0; i < n - 1; i++) {
+  	int smaller_position = i;
+    for (int j = i + 1; j < n; j++) if (v[j] < v[smaller_position]) smaller_position = j;
+  	if (smaller_position != i) swap(v[i], v[smaller_position]);
   }
+}
+
+int main() {
+	int ar[] = { 33,23,10,9,1 };
+	
+	for (int i = 0; i < 5; i++) cout << ar[i] << " ";
+	cout << endl;
+	
+	selection_sort(ar, 5);
+	
+	for (int i = 0; i < 5; i++) cout << ar[i] << " ";
+	cout << endl;
+	
+	return 0;
 }
