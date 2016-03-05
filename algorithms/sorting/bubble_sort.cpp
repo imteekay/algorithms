ca5d231cb64328ea5b8f408dbe7868f675ff77b6
Leandro Tk <leandrodtk@hotmail.com>
2016-03-05 00:08:21 -0300

    Fixed bubble sort implementation

--- algorithms/sorting/bubble_sort.cpp
+++ algorithms/sorting/bubble_sort.cpp
@@ -1,4 +1,7 @@
-#define swap(a, b) { int x=a; a=b; b=x  }
+#include <iostream>
+using namespace std;
+
+#define swap(a, b) { int x=a; a=b; b=x; }
 
 void bubble_sort(int v[], int n) {
   for (int i = 1; i <= n - 1; i++) {
@@ -6,4 +9,18 @@ void bubble_sort(int v[], int n) {
       if (v[j] > v[j+1]) swap(v[j], v[j+1]);
     }
   }
+}
+
+int main() {
+	int ar[] = { 33,23,10,9,1 };
+	
+	for (int i = 0; i < 5; i++) cout << ar[i] << " ";
+	cout << endl;
+	
+	bubble_sort(ar, 5);
+	
+	for (int i = 0; i < 5; i++) cout << ar[i] << " ";
+	cout << endl;
+	
+	return 0;
 }
