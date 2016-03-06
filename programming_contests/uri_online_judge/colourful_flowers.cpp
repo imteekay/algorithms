caf32f17e917084fffed9ab43b9c2e3fba0a6af9
Leandro Tk <leandrodtk@hotmail.com>
2016-03-05 22:31:45 -0300

    Update colourful_flowers.cpp

--- programming_contests/uri_online_judge/colourful_flowers.cpp
+++ programming_contests/uri_online_judge/colourful_flowers.cpp
@@ -6,16 +6,18 @@
 
 using namespace std;
 
-double get_greater(int l1, int l2, int l3) {
-	if (l1 > l2 && l1 > l3) return l1;
-	else if (l2 > l3) return l2;
-	else return l3;
+double get_greater_radius(int a, int b, int c) {
+	double bottom = sqrt((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c));
+	return (a * b * c) / (bottom);
 }
 
 double get_triangle_area(int l1, int l2, int l3) {
-	if (l1 > l2 && l1 > l3) return l2 * l3 / 2;
-	else if (l2 > l3) return l1 * l3 / 2;
-	else return l1 * l2 / 2;
+	double perim = (l1 + l2 + l3) / 2.0;
+	double part1 = perim - l1;
+	double part2 = perim - l2;
+	double part3 = perim - l3;
+	double area = sqrt(perim * part1 * part2 * part3);
+	return area;
 }
 
 double get_smaller_circunference_radius(double triangle_area, double triangle_perimeter) {
@@ -25,14 +27,14 @@ double get_smaller_circunference_radius(double triangle_area, double triangle_pe
 int main() {
 	
 	double l1, l2, l3;
+	const double PI = 3.1415926535897;
 	
 	while (scanf("%lf %lf %lf", &l1, &l2, & l3) != EOF) {
-		double greater = get_greater(l1, l2, l3);
-		double radius = greater / 2;
-		double circunference_area = M_PI * radius * radius;
+		double radius = get_greater_radius(l1, l2, l3);
+		double circunference_area = PI * radius * radius;
 		double triangle_area = get_triangle_area(l1, l2, l3);
 		double smaller_circunference_radius = get_smaller_circunference_radius(triangle_area, l1+l2+l3);
-		double smaller_circunference_area = M_PI * smaller_circunference_radius * smaller_circunference_radius;
+		double smaller_circunference_area = PI * smaller_circunference_radius * smaller_circunference_radius;
 		cout << fixed << setprecision(4) << circunference_area - triangle_area << " " << triangle_area - smaller_circunference_area << " " << smaller_circunference_area << endl;
 	}
 	
