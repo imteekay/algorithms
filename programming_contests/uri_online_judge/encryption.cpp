7a28ce05736db2fadfddd9055323fcf9443cb825
Leandro Tk <leandrodtk@hotmail.com>
2016-03-08 00:03:32 -0300

    Started to implement empcrypt algorithm

---
+++ programming_contests/uri_online_judge/encryption.cpp
@@ -0,0 +1,25 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1024
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin >> n;
+
+	while (n--) {
+		string password;
+		cin >> password;
+
+		// first step: http://en.cppreference.com/w/cpp/language/ascii
+
+		// second step: reverse string
+
+		// third step: left
+	}
+
+	return 0;
+}
