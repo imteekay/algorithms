27c123b36cadeb2caf8987ea05010b88237d5a4a
Leandro Tk <leandrodtk@hotmail.com>
2016-03-14 22:28:33 -0300

    Improved angular controllers and added style for page

--- js/algorithms_controller.js
+++ js/algorithms_controller.js
@@ -1,5 +1,47 @@
 var app = angular.module('algorithm', []);
 
+app.controller('CppFeaturesController', ['$scope', function($scope) {
+
+  $scope.cppFeatures = [
+    {
+      'title': 'C++ Vector: A pretty simple guide',
+      'hasMediumPost': true,
+      'mediumPostUrl': 'https://medium.com/@leandrotk_/c-standard-template-library-stl-vector-a-pretty-simple-guide-d2b64184d50b#.5aquqmuns',
+      'hasGithubCode': true,
+      'githubCodeUrl': 'https://github.com/LeandroTk/algorithms/blob/gh-pages/cpp/standard_template_library/vectors.cpp',
+      'studied': true
+    },
+
+    {
+      'title': 'C++ String: A pretty simple guide',
+      'hasMediumPost': false,
+      'mediumPostUrl': '',
+      'hasGithubCode': false,
+      'githubCodeUrl': '',
+      'studied': false
+    },
+
+    {
+      'title': 'C++ Map: A pretty simple guide',
+      'hasMediumPost': false,
+      'mediumPostUrl': '',
+      'hasGithubCode': false,
+      'githubCodeUrl': '',
+      'studied': false
+    },
+
+    {
+      'title': 'C++ Sorting: A pretty simple guide',
+      'hasMediumPost': false,
+      'mediumPostUrl': '',
+      'hasGithubCode': false,
+      'githubCodeUrl': '',
+      'studied': false
+    },
+  ];
+
+}]);
+
 app.controller('AlgorithmsController', ['$scope', function($scope) {
 
   $scope.algorithms = [
