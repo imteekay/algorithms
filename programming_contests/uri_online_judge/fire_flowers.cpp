4a87647d8904ebec5ff47a142e8564c3420ed12c
Leandro Tk <leandrodtk@hotmail.com>
2016-03-21 15:37:37 -0300

    Update fire_flowers.cpp

--- programming_contests/uri_online_judge/fire_flowers.cpp
+++ programming_contests/uri_online_judge/fire_flowers.cpp
@@ -15,12 +15,16 @@ int main() {
   int r1, x1, y1, r2, x2, y2;
 
   while (cin >> r1 >> x1 >> y1 >> r2 >> x2 >> y2) {
-    int total_radius = r1 + r2;
     int distance_x = to_positive(x1 - x2);
     int distance_y = to_positive(y1 - y2);
     double total_distance = sqrt(pow(distance_x, 2) + pow(distance_y, 2));
-    if (total_distance > total_radius) cout << "MORTO" << endl;
-    else cout << "RICO" << endl;
+
+    if (r1 < r2) {
+      cout << "MORTO" << endl;
+    } else {
+      if (r1 >= r2 + total_distance) cout << "RICO" << endl;
+      else cout << "MORTO" << endl;
+    }
   }
 
   return 0;
