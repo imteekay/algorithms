15bc13c47d98435dc78781bb83b0f4635c8b95ca
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-03-21 18:29:53 -0300

    Solved diet plan problem

--- programming_contests/uri_online_judge/diet_plan.cpp
+++ programming_contests/uri_online_judge/diet_plan.cpp
@@ -11,35 +11,36 @@ int main() {
 
 	int n;
 	string diet, breakfest, lunch;
-	vector< pair<char, bool> > valid_chars;
 
 	cin >> n;
+	cin.ignore();
 
 	while (n--) {
-		bool is_cheating = true;
-		cin >> diet >> breakfest >> lunch;
+		vector< pair<char, bool> > valid_chars;
+		bool is_cheating = false;
+		getline(cin, diet);
+		getline(cin, breakfest);
+		getline(cin, lunch);
 
-		for (int i = 0; i < breakfest.size(); i++) valid_chars.push_back(make_pair(breakfest[i], true));
-		for (int i = 0; i < lunch.size(); i++) valid_chars.push_back(make_pair(lunch[i], true));
+		for (int i = 0; i < diet.size(); i++) valid_chars.push_back(make_pair(diet[i], true));
 
 		int find_char;
 
 		for (int i = 0; i < breakfest.size(); i++) {
 			find_char = diet.find(breakfest[i]);
 			if (find_char == -1) {
-				is_cheating = false;
+				is_cheating = true;
 				break;
 			} else {
 				for (int j = 0; j < valid_chars.size(); j++) if (valid_chars[j].first == breakfest[i]) valid_chars[j].second = false;
 			}
 		}
 
-
 		if (!is_cheating) {
 			for (int i = 0; i < lunch.size(); i++) {
 				find_char = diet.find(lunch[i]);
 				if (find_char == -1) {
-					is_cheating = false;
+					is_cheating = true;
 					break;
 				} else {
 					for (int j = 0; j < valid_chars.size(); j++) if (valid_chars[j].first == lunch[i]) valid_chars[j].second = false;
@@ -56,8 +57,6 @@ int main() {
 		} else {
 			cout << "CHEATER" << endl;
 		}
-
-		valid_chars.clear();
 	}
 
 	return 0;
