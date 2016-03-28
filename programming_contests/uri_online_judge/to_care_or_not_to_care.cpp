36deb8358b43392309bc23c168f520318277ae45
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-03-28 17:16:39 -0300

    improving algorithm for to care problem

--- programming_contests/uri_online_judge/to_care_or_not_to_care.cpp
+++ programming_contests/uri_online_judge/to_care_or_not_to_care.cpp
@@ -7,13 +7,16 @@
 
 using namespace std;
 
-void add_zero_values(vector<int> &v) {
-  int total_num_of_values = v.size();
-  int num_needed = 0;
-  for (int i = 4; i < total_num_of_values; i += 4) num_needed += i;
-  num_needed += 4;
-  int difference = num_needed - total_num_of_values;
-  for (int j = 0; j < difference; j++) v.push_back(0);
+void add_zero_values(vector<int> &v1, vector<int> &v2) {
+  int v1_size = v1.size(), v2_size = v2.size(), difference;
+
+  if (v1_size > v2_size) {
+    difference = v1_size - v2_size;
+    for (int j = 0; j < difference; j++) v2.push_back(0);
+  } else if (v1_size < v2_size) {
+    difference = v2_size - v1_size;
+    for (int j = 0; j < difference; j++) v1.push_back(0);
+  }
 }
 
 vector<int> to_binary_number(int n) {
@@ -23,15 +26,13 @@ vector<int> to_binary_number(int n) {
     n /= 2;
   }
 
-  add_zero_values(v);
-  reverse(v.begin(), v.end());
   return v;
 }
 
 int to_decimal(vector<int> &v) {
   int decimal_num = 0;
   int size = v.size();
-  for (int i = 0; i < size; i++) if (v[i] == 1) decimal_num += pow(2, size-i);
+  for (int i = 0; i < size; i++) if (v[i] == 1) decimal_num += pow(2, size-i-1);
   return decimal_num;
 }
 
@@ -39,9 +40,13 @@ int main() {
 
   int num1, num2;
 
-  while (cin >> num1 >> num2) {
+  while (scanf("%d %d", &num1, &num2) != EOF) {
     vector<int> binary_num1 = to_binary_number(num1);
     vector<int> binary_num2 = to_binary_number(num2);
+    add_zero_values(binary_num1, binary_num2);
+    reverse(binary_num1.begin(), binary_num1.end());
+    reverse(binary_num2.begin(), binary_num2.end());
+
     vector<int> new_vec;
 
     for (int i = 0; i < binary_num2.size(); i++) {
