89cdacc44048f01c5a6701e510d983beb07677d1
Leandro Tk <leandrodtk@hotmail.com>
2016-04-05 22:45:09 -0300

    Sstarted to to implementt solution

---
+++ programming_contests/uri_online_judge/the_wrath_of_khan.cpp
@@ -0,0 +1,24 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1964
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int c, va, vb, t, d, da, db;
+
+	cin >> c >> va >> vb >> t >> d;
+
+	va /= 100;
+	va *= 60;
+	vb /= 100;
+	vb *= 60;
+
+	da = va * t;
+	db = vb * t;
+
+	if ()
+
+	return 0;
+}
