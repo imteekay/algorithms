f58bd08a87e56140edda8135a8d792be793f571b
Leandro Tk <leandrodtk@hotmail.com>
2016-04-06 23:12:02 -0300

    Fixing max subsequence sum

--- algorithms/maximum_subsequence_sum/maximum_subsequence_sum_1.cpp
+++ algorithms/maximum_subsequence_sum/maximum_subsequence_sum_1.cpp
@@ -6,24 +6,30 @@ int maximum_subsequence_sum(int ar[], int n) {
 	int sum = 0, result = 0;
 
 	for (int i = 0; i < n; i++) {		
-		if (sum + ar[i] > 0) {
-			sum += ar[i];
-		} else {
-			if (sum > result) result = sum;
-			sum = 0;
-		}
+		if (sum + ar[i] > 0) sum += ar[i];
+		else sum = 0;			
+		if (sum > result) result = sum;
 	}
 
 	return result;
 }
 
 int main() {
-	
-	int ar[] = { 1, -3, 2, -5, 7, 6, -1, -4, 11, -23 };
-	int n = 10;
+	int n, result;
 
-	int result = maximum_subsequence_sum(ar, n);
+	int ar1[] = { 1, -3, 2, -5, 7, 6, -1, -4, 11, -23 };
+	n = 10;
+	result = maximum_subsequence_sum(ar1, n);
+	cout << result << endl;
 
+	int ar2[] = { 1, -3, 2, -5, 7 };
+	n = 5;
+	result = maximum_subsequence_sum(ar2, n);
+	cout << result << endl;
+
+	int ar3[] = { 1, -3, 6, -5, 7 };
+	n = 5;
+	result = maximum_subsequence_sum(ar3, n);
 	cout << result << endl;
 
 	return 0;
