cb6342adc88c12091a58ab21665dbc9bc3c5bf04
Leandro Tk <leandrodtk@hotmail.com>
2016-04-14 00:06:41 -0300

    SOlved trinomial triangle

---
+++ programming_contests/uri_online_judge/trinomial_triangle.cpp
@@ -0,0 +1,18 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1795
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin >> n;
+
+	long long int result = pow(3, n);
+
+	cout << result << endl;
+
+	return 0;
+}
