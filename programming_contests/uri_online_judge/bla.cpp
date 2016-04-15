12ffb1ead0fcdd173617f2659ee7f4fbd658bf6e
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-04-15 18:52:11 -0300

    Added bla

---
+++ programming_contests/uri_online_judge/bla.cpp
@@ -0,0 +1,20 @@
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+  string "QWERTYUIOP[]-ASDFGHJKL;'ZXCVBNM,./";
+
+  string phrase;
+  cin >> phrase;
+
+  for (int i = 0; i < phrase.size(); i++) {
+    if ()
+  }
+
+  return 0;
+}
