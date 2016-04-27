92f9d5d4396230272d907ebf08656195ced6a344
Leandro Tk <leandrodtk@hotmail.com>
2016-04-26 23:37:01 -0300

    Solved hours and minutes problem

--- programming_contests/uri_online_judge/hours_and_minutes.cpp
+++ programming_contests/uri_online_judge/hours_and_minutes.cpp
@@ -4,41 +4,13 @@
 
 using namespace std;
 
-int degree_to_minutes(int degree) {
-  return 60 * degree / 360;
-}
+int main() {  
+  int angle;
 
-int main() {
-
-  int n;
-
-  while (cin >> n) {
-
+  while (scanf("%i", &angle) != EOF) {
+    if (angle % 6 == 0) cout << "Y" << endl;
+    else cout << "N" << endl;
   }
 
   return 0;
 }
-
-/*
-  0 11 --> 11
-  1 12 --> 11
-  1 23 --> 22
-  2 24 --> 22
-  2 35 --> 33
-  3 36 --> 33
-  3 47 --> 44
-  4 48 --> 44
-  4 59 --> 55
-  5 60 --> 55
-
-  5 11 --> 6
-  6 12 --> 6
-  6 23 --> 17
-  7 24 --> 17
-  7 35 --> 28
-  8 36 --> 28
-  8 47 --> 39
-  9 48 --> 39
-  9 59 --> 50
-  10 60 --> 50
-*/
