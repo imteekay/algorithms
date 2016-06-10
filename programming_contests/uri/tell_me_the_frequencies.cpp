b4c8487e18b276a9a9e4b4df8ea7b8cc2cf1abbc
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-06-10 16:08:07 -0300

    Implementing tell me the frequencies problem

---
+++ programming_contests/uri/tell_me_the_frequencies.cpp
@@ -0,0 +1,45 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1251
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int toASCII(char charac) {
+  int newCharac = int(charac);
+  return newCharac;
+}
+
+bool compareByFrequency(const pair<int, int> &item1, const pair<int, int> &item2) {
+  return item1.second < item2.second;
+}
+
+int main() {
+  char charac;
+  string text;
+  bool first = true;
+
+  while (getline(cin, text)) {
+    map<int, int> m;
+    if (first) first = false;
+    else cout << endl;
+
+    for (int i = 0; i < text.size(); i++) {
+      charac = toASCII(text[i]);
+      if (m.find(charac) != m.end()) m[charac]++;
+      else m[charac] = 1;
+    }
+
+    vector< pair<int, int> > vec(m.begin(), m.end());
+    sort(vec.begin(), vec.end(), compareByFrequency);
+
+    for (int i = 0; i < vec.size(); ++i) {
+      cout << vec[i].first << " " << vec[i].second << endl;
+    }
+  }
+
+  return 0;
+}
