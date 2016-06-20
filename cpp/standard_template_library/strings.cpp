5522bcdc2a68715876ed4f68a0ec05e017f4f228
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-06-20 10:18:23 -0300

    Organizing file

--- cpp/standard_template_library/strings.cpp
+++ cpp/standard_template_library/strings.cpp
@@ -5,55 +5,55 @@ using namespace std;
 
 int main() {
 
-		// defining strings
-		string string1 = "hello world";
-		string string2;
+	// defining strings
+	string string1 = "hello world";
+	string string2;
 
-		// printing strings
-		cout << string1 << endl;
+	// printing strings
+	cout << string1 << endl;
 
-		// finding letter(s) in a string
-		// return npos --> -1 if don't find the element
-		cout << string1.find("e") << endl;
+	// finding letter(s) in a string
+	// return npos --> -1 if don't find the element
+	cout << string1.find("e") << endl;
 
-		// using input for string
-		cin >> string2;
-		cout << string2 << endl;
+	// using input for string
+	cin >> string2;
+	cout << string2 << endl;
 
-		// size
-		cout << string1.size() << endl;
-		cout << string2.size() << endl;
+	// size
+	cout << string1.size() << endl;
+	cout << string2.size() << endl;
 
-		// substrings
-		cout << string1.substr(0, 5) << endl;
-		cout << string1.substr(6, string1.size() - 1) << endl;
+	// substrings
+	cout << string1.substr(0, 5) << endl;
+	cout << string1.substr(6, string1.size() - 1) << endl;
 
-		string palavra = "lalala.lololo";
-		cout << palavra.substr(6, 1) << endl;
-		cout << palavra.substr(0, 6 - 0) << endl;
-		cout << palavra.substr(6+1, palavra.size()-6) << endl;
+	string palavra = "lalala.lololo";
+	cout << palavra.substr(6, 1) << endl;
+	cout << palavra.substr(0, 6 - 0) << endl;
+	cout << palavra.substr(6+1, palavra.size()-6) << endl;
 
-		// string to integer
-		string um = "1";
-  	cout << stoi(um) << " - deu certo!" << endl;
+	// string to integer
+	string um = "1";
+	cout << stoi(um) << " - deu certo!" << endl;
 
-  	string vinte = "20";
-		cout << stoi(vinte) << " - deu certo.. de novo!" << endl;
+	string vinte = "20";
+	cout << stoi(vinte) << " - deu certo.. de novo!" << endl;
 
-		// last element of string
-		string word = "Leandro";
-		cout << palavra.back() << endl;
+	// last element of string
+	string word = "Leandro";
+	cout << palavra.back() << endl;
 
-		// we can iterate a string: each char
-		for (int i = 0; i < word; i++) cout << word[i] << " ";
-		cout << endl;
+	// we can iterate a string: each char
+	for (int i = 0; i < word; i++) cout << word[i] << " ";
+	cout << endl;
 
-		// compare string letter with char
-		for (int i = 0; i < word.size(); i++) if (word[i] == 'a') cout << word[i] << " ";
-		cout << endl;
+	// compare string letter with char
+	for (int i = 0; i < word.size(); i++) if (word[i] == 'a') cout << word[i] << " ";
+	cout << endl;
 
-		// use cin to get string
-		// use getline(cin, string) to get a line of strings
+	// use cin to get string
+	// use getline(cin, string) to get a line of strings
 
-		return 0;
+	return 0;
 }
