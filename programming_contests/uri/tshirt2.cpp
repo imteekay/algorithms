6307c7ef408238e0080f431f3eb641e48946a898
Leandro Tk <leandrodtk@hotmail.com>
2016-06-20 22:41:59 -0300

    Solved tshirts problem

--- programming_contests/uri/tshirt2.cpp
+++ programming_contests/uri/tshirt2.cpp
@@ -9,26 +9,61 @@ using namespace std;
 
 struct tshirt {
   string color;
-  char tshirtSize;
+  string tshirtSize;
 };
 
-int main() {
-  int n;
-  string name, tshirt, color;
+int clothesFound(vector< pair< tshirt, vector<string> > > &clothes, string tshirtSize, string color) {
+  for (int i = 0; i < clothes.size(); i++) {
+    if (clothes[i].first.tshirtSize == tshirtSize && clothes[i].first.color == color) 
+      return i;
+  }
 
-  while (cin >> n && n != 0) {
-    cin.ignore();
+  return -1;
+}
+
+bool compareClothes(const pair< tshirt, vector<string> > &p1, const pair< tshirt, vector<string> > &p2) {
+  if (p1.first.color < p2.first.color) return true;
+  else if (p1.first.color > p2.first.color) return false;
+  else {
+    if (p1.first.tshirtSize >= p2.first.tshirtSize) return true;
+    else return false;
+  }
+}
+
+int main() {
+  int n, found;
+  string name, tshirtSize, color;
+
+  while (cin >> n && n != 0) {    
     vector< pair< tshirt, vector<string> > > clothes;
     vector<string> v;
 
     while (n--) {
+      cin.ignore();
       getline(cin, name);
-      cin >> tshirt >> color;
+      cin >> color >> tshirtSize;
+      tshirt t;
+      t.tshirtSize = tshirtSize;
+      t.color = color;
+      found = clothesFound(clothes, tshirtSize, color);
 
-
+      if (found != -1) {
+        clothes[found].second.push_back(name);
+      } else {
+        vector<string> names;
+        names.push_back(name);
+        clothes.push_back(make_pair(t, names));
+      }
     }
 
+    sort(clothes.begin(), clothes.end(), compareClothes);
 
+    for (int i = 0; i < clothes.size(); i++) {
+      sort(clothes[i].second.begin(), clothes[i].second.end());
+      for (int j = 0; j < clothes[i].second.size(); j++) {
+        cout << clothes[i].first.color << " " << clothes[i].first.tshirtSize << " " << clothes[i].second[j] << endl;
+      }
+    }
   }
 
   return 0;
