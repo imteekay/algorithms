4aedba2eb09631d4dbc776b7c25fd561515893b1
Leandro Tk <leandrodtk@hotmail.com>
2016-07-31 12:07:07 -0300

    Create the_pythagorean_theorem.cpp

---
+++ programming_contests/uri/need_submition/the_pythagorean_theorem.cpp
@@ -0,0 +1,46 @@
+
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+#define min(m,n) ((m) < (n) ? (m) : (n))
+
+int mdc(int m, int n){
+	int a = min(abs(m), abs(n));;
+	while (m % a != 0 || n % a != 0) a--;
+ 	return a;
+}
+
+int main(){
+    int x, y, z;
+    while(cin >> x >> y >> z) {
+        int hip, c1, c2;
+        if(z >= y && z >= x) { 
+        	hip = z; 
+        	c1 = y;
+        	c2 = x;
+        } else if (y >= z && y >= x) {
+        	hip = y; 
+        	c1 = z;
+        	c2 = x;
+        } else if (x >= z && x >= y) { 
+        	hip = x; 
+        	c1 = z; 
+        	c2 = y;
+        }
+
+        printf("tripla");
+	    
+	    if(hip * hip == (c1 * c1 + c2 * c2)) {
+	        printf(" pitagorica");
+	        if(mdc(mdc(x,y), z) == 1) printf(" primitiva");
+	    }
+	    
+    	cout << endl;
+    }
+
+    return 0;
+}
