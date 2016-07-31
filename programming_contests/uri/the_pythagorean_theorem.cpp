c5c7834ae142d214eb4d7cf866d92947b3645141
Leandro Tk <leandrodtk@hotmail.com>
2016-07-31 12:06:23 -0300

    Update the_pythagorean_theorem.cpp

--- programming_contests/uri/the_pythagorean_theorem.cpp
+++ programming_contests/uri/the_pythagorean_theorem.cpp
@@ -1,48 +1,46 @@
-// https://www.urionlinejudge.com.br/judge/en/problems/view/1582
+
 
 #include <iostream>
-#include <stdlib.h>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
+#define min(m,n) ((m) < (n) ? (m) : (n))
+
 int mdc(int m, int n){
-  int a = min(abs(m), abs(n));  
-  while (m % a != 0 || n % a!= 0) a--;  
-  return a;
- }
+	int a = min(abs(m), abs(n));;
+	while (m % a != 0 || n % a != 0) a--;
+ 	return a;
+}
 
-int main() {
+int main(){
+    int x, y, z;
+    while(cin >> x >> y >> z) {
+        int hip, c1, c2;
+        if(z >= y && z >= x) { 
+        	hip = z; 
+        	c1 = y;
+        	c2 = x;
+        } else if (y >= z && y >= x) {
+        	hip = y; 
+        	c1 = z;
+        	c2 = x;
+        } else if (x >= z && x >= y) { 
+        	hip = x; 
+        	c1 = z; 
+        	c2 = y;
+        }
 
-	int n1, n2, n3;
+        printf("tripla");
+	    
+	    if(hip * hip == (c1 * c1 + c2 * c2)) {
+	        printf(" pitagorica");
+	        if(mdc(mdc(x,y), z) == 1) printf(" primitiva");
+	    }
+	    
+    	cout << endl;
+    }
 
-	while (cin >> n1 >> n2 >> n3) {
-		int hip, cat1, cat2;
-
-		if (n1 > n2 && n1 > n3) {
-			hip = n1;
-			cat1 = n2;
-			cat2 = n3;
-		} else if (n2 > n3) {
-			hip = n2;
-			cat1 = n1;
-			cat2 = n3;
-		} else {
-			hip = n3;
-			cat1 = n2;
-			cat2 = n1;
-		}
-
-		hip = hip * hip;
-		cat1 = cat1 * cat1;
-		cat2 = cat2 * cat2;
-
-		if (hip == cat1 + cat2) {
-			if (mdc(mdc(hip, cat1), cat2) == 1) cout << "tripla pitagorica primitica" << endl;
-			else cout << "tripla pitagorica" << endl;
-		} else {
-			cout << "tripla" << endl;
-		}
-	}
-
-	return 0;
+    return 0;
 }
