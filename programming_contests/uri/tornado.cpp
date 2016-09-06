cc8f675308a4bbe8a2d4705264965048368ce3f4
Leandro Tk <leandro.kinoshita@codus.com.br>
2016-09-06 13:32:57 -0300

    Starting to solve tornado problem

---
+++ programming_contests/uri/tornado.cpp
@@ -0,0 +1,29 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1266
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int n;
+  string tornado;
+  char post;
+
+  while (cin >> n && n != 0) {
+    tornado = "";
+
+    for (int i = 0; i < n; i++) {
+      cin >> post;
+      tornado += post;
+    }
+
+    if (tornardo.begin() + tornado.back() > 0) {
+
+    } else {
+
+    }
+  }
+
+  return 0;
+}
