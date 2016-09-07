44bc58e137dfcad0c694b8e694fdf907ab0a7215
Leandro Tk <leandrodtk@hotmail.com>
2016-09-07 01:03:09 -0300

    Solving tornado problem

--- programming_contests/uri/tornado.cpp
+++ programming_contests/uri/tornado.cpp
@@ -5,6 +5,30 @@
 
 using namespace std;
 
+int sumNumberOfNeededPosts(string tornado) {
+  int neededPostsNumber = 0, sum = 0, n, numberOfBrokenPosts = 0;
+
+  for (int i = 0; i < tornado.size(); i++) {
+    n = tornado[i] - '0';
+    sum += n;
+
+    if (sum > 0) {
+      if (numberOfBrokenPosts % 2 == 0) {
+        neededPostsNumber += numberOfBrokenPosts / 2;
+      } else {
+        neededPostsNumber += (numberOfBrokenPosts-1) / 2;
+      }
+
+      numberOfBrokenPosts = 0;
+      sum = 0;
+    } else {
+      numberOfBrokenPosts++;
+    }
+  }
+
+  return neededPostsNumber;
+}
+
 int main() {
   int n;
   string tornado;
@@ -18,11 +42,21 @@ int main() {
       tornado += post;
     }
 
-    if (tornado.begin() == "1" || tornado.back() == "1") {
-      cout << tornado << endl;
-    } else {
+    if (tornado[0] == '0' && tornado[tornado.size()-1] == '0') {
+      int index;
 
+      for (int i = tornado.size()-1; i >= 0; i--) {
+        if (tornado[i] == '1') {
+          index = i;
+          break;
+        }
+      }
+
+      tornado += tornado;
+      tornado = tornado.substr(index, tornado.size());
     }
+
+    cout << sumNumberOfNeededPosts(tornado) << endl;
   }
 
   return 0;
