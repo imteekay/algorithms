6123e22275352ca7a6e8ea8e4444ea8e8928d851
Leandro Tk <leandrodtk@hotmail.com>
2016-11-08 18:56:19 -0200

    Fixing algorithm

--- programming_contests/uri/password_validator.cpp
+++ programming_contests/uri/password_validator.cpp
@@ -2,23 +2,28 @@
 
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
 bool validateString(string s) {
 	string alphabetUppercase = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
 	string alphabetLowercase = "abcdefghijklmnopqrstuvxwyz";
-	sort(s.begin(), s.end());
-	if (alphabetUppercase.find(s[0]) != string::npos && alphabetLowercase.find(s[s.size()-1]) != string::npos) return true;
-	return false;
+	string numbers = "0123456789";
+	bool hasUppercasedChar = false, hasLowercasedChar = false, hasNumber = false;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (alphabetUppercase.find(s[i]) != string::npos) hasUppercasedChar = true;
+		if (alphabetLowercase.find(s[i]) != string::npos) hasLowercasedChar = true;
+		if (numbers.find(s[i]) != string::npos) hasNumber = true;
+	}
+
+	return hasUppercasedChar && hasLowercasedChar && hasNumber;
 }
 
 bool validatePunctuation(string s) {
-	if (s.find(".") == string::npos &&
-			s.find("á") == string::npos &&
+	if (s.find("á") == string::npos &&
 			s.find("é") == string::npos &&
-			s.find("é") == string::npos &&
+			s.find("í") == string::npos &&
 			s.find("ó") == string::npos &&
 			s.find("ú") == string::npos &&
 			s.find("Á") == string::npos &&
@@ -26,6 +31,70 @@ bool validatePunctuation(string s) {
 			s.find("Í") == string::npos &&
 			s.find("Ó") == string::npos &&
 			s.find("Ú") == string::npos &&
+			s.find("â") == string::npos &&
+			s.find("ê") == string::npos &&
+			s.find("î") == string::npos &&
+			s.find("ô") == string::npos &&
+			s.find("û") == string::npos &&
+			s.find("Â") == string::npos &&
+			s.find("Ê") == string::npos &&
+			s.find("Î") == string::npos &&
+			s.find("Ô") == string::npos &&
+			s.find("Û") == string::npos &&
+			s.find("ã") == string::npos &&
+			s.find("ẽ") == string::npos &&
+			s.find("ĩ") == string::npos &&
+			s.find("õ") == string::npos &&
+			s.find("ũ") == string::npos &&
+			s.find("Ã") == string::npos &&
+			s.find("Ẽ") == string::npos &&
+			s.find("Ĩ") == string::npos &&
+			s.find("Õ") == string::npos &&
+			s.find("Ũ") == string::npos &&
+			s.find("à") == string::npos &&
+			s.find("è") == string::npos &&
+			s.find("ì") == string::npos &&
+			s.find("ò") == string::npos &&
+			s.find("ù") == string::npos &&
+			s.find("À") == string::npos &&
+			s.find("È") == string::npos &&
+			s.find("Ì") == string::npos &&
+			s.find("Ò") == string::npos &&
+			s.find("Ù") == string::npos &&
+			s.find(".") == string::npos &&
+			s.find(",") == string::npos &&
+			s.find(";") == string::npos &&
+			s.find("!") == string::npos &&
+			s.find("?") == string::npos &&
+			s.find(":") == string::npos &&
+			s.find("(") == string::npos &&
+			s.find(")") == string::npos &&
+			s.find("[") == string::npos &&
+			s.find("]") == string::npos &&
+			s.find("{") == string::npos &&
+			s.find("}") == string::npos &&
+			s.find("-") == string::npos &&
+			s.find("\"") == string::npos &&
+			s.find("\\") == string::npos &&
+			s.find("/") == string::npos &&
+			s.find("...") == string::npos &&
+			s.find("'") == string::npos &&
+			s.find("^") == string::npos &&
+			s.find("`") == string::npos &&
+			s.find("~") == string::npos &&
+			s.find("+") == string::npos &&
+			s.find("-") == string::npos &&
+			s.find("=") == string::npos &&
+			s.find("_") == string::npos &&
+			s.find("*") == string::npos &&
+			s.find("&") == string::npos &&
+			s.find("¨") == string::npos &&
+			s.find("%") == string::npos &&
+			s.find("$") == string::npos &&
+			s.find("#") == string::npos &&
+			s.find("@") == string::npos &&
+			s.find(">") == string::npos &&
+			s.find("<") == string::npos &&
 			s.find(" ") == string::npos) return true;
 
 	return false;
