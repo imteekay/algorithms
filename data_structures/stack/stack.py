2e7e2831bd4c86592f0ef64692f21ec01215c7a6
Leandro Tk <leandrodtk@hotmail.com>
2017-06-08 23:21:34 -0300

    Implemented stack DS

---
+++ data_structures/stack/stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def is_empty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
