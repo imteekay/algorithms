9ed8c2f0a20f635143890d4989ea786a4975f38d
Leandro Tk <leandrodtk@hotmail.com>
2017-06-12 23:09:23 -0300

    Queue implementation in python

---
+++ data_structures/queue/queue.py
@@ -0,0 +1,15 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def is_empty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items
