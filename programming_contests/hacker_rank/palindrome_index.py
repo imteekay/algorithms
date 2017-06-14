7607108efbbbae794a1d2eb25a221896ca4f43f5
Leandro Tk <leandrodtk@hotmail.com>
2017-06-14 00:01:11 -0300

    Solved palindrome index problem

---
+++ programming_contests/hacker_rank/palindrome_index.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com/challenges/palindrome-index
+
+import sys
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+def palindromeIndex(s):
+    for i in range(len(s)):
+        if is_palindrome(s[i+1:len(s)-i]):
+            return i
+
+        if is_palindrome(s[i:len(s)-i-1]):
+            return len(s)-i-1
+
+q = int(raw_input().strip())
+
+for num in range(q):
+    s = raw_input().strip()
+    result = palindromeIndex(s)
+    print(result)
