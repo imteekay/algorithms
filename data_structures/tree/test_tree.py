070ca81a5eb393eb60783195c57a29e6425294ca
Leandro Tk <leandrodtk@hotmail.com>
2017-06-24 19:37:58 -0300

    Implemented Binary Tree and test it

--- data_structures/tree/test_tree.py
+++ data_structures/tree/test_tree.py
@@ -1,4 +1,29 @@
 from tree import BinaryTree
 
-tree = BinaryTree(1)
-print(tree)
+tree = BinaryTree('a')
+tree.insert_left('b')
+tree.get_left_child().insert_right('d')
+tree.insert_right('c')
+tree.get_right_child().insert_left('e')
+tree.get_right_child().insert_right('f')
+
+a = tree.get_root_value()
+b = tree.get_left_child().get_root_value()
+c = tree.get_right_child().get_root_value()
+d = tree.get_left_child().get_right_child().get_root_value()
+e = tree.get_right_child().get_left_child().get_root_value()
+f = tree.get_right_child().get_right_child().get_root_value()
+
+print("               |%s|" %(a))
+print("             /     \\")
+print("           |%s|     |%s|" %(b, c))
+print("              \\   /   \\")
+print("             |%s| |%s| |%s|" %(d, e, f))
+
+# ------- Building this Tree -------
+#
+#               |a|
+#             /    \
+#          |b|     |c|
+#           \     /  \
+#           |d| |e|  |f|
--- data_structures/tree/tree.py
+++ data_structures/tree/tree.py
@@ -3,3 +3,28 @@ class BinaryTree:
         self.value = value
         self.left_child = None
         self.right_child = None
+
+    def insert_left(self, value):
+        if self.left_child == None:
+            self.left_child = BinaryTree(value)
+        else:
+            new_node = BinaryTree(value)
+            new_node.left_child = self.left_child
+            self.left_child = new_node
+
+    def insert_right(self, value):
+        if self.right_child == None:
+            self.right_child = BinaryTree(value)
+        else:
+            new_node = BinaryTree(value)
+            new_node.right_child = self.right_child
+            self.right_child = new_node
+
+    def get_left_child(self):
+        return self.left_child
+
+    def get_right_child(self):
+        return self.right_child
+
+    def get_root_value(self):
+        return self.value
