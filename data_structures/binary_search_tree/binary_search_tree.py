c156597e3be6eb782ffa6ee1101c0e748a24e429
Leandro Tk <leandrodtk@hotmail.com>
2017-06-27 22:57:14 -0300

    Started to implement binary search tree insert and find

--- data_structures/binary_search_tree/binary_search_tree.py
+++ data_structures/binary_search_tree/binary_search_tree.py
@@ -9,3 +9,37 @@ class Node:
 
     def set(self, new_value):
         self.value = new_value
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root == None:
+            self.__set_root(value)
+        else:
+            self.insert_node(self.root, value)
+
+    def insert_node(self, current_node, value):
+        if value <= current_node.value and current_node.left_child:
+            self.insert_node(current_node.left_child, value)
+        elif value <= current_node.value:
+            current_node.left_child = Node(value)
+        elif value > current_node.value and current_node.right_child:
+            self.insert_node(current_node.right_child, value)
+        else:
+            current_node.right_child = Node(value)
+
+    def find(self, value):
+        return self.find_node(self.root, value)
+
+    def find_node(self, current_node, value):
+        if value < current_node.value and current_node.left_child:
+            self.find_node(current_node.left_child, value)
+        if value > current_node.value and current_node.right_child:
+            self.find_node(current_node.right_child, value)
+
+        return value == current_node.value
+
+    def __set_root(self, value):
+        self.root = Node(value)
---
+++ data_structures/binary_search_tree/test_binary_search_tree.py
@@ -0,0 +1,8 @@
+from binary_search_tree import *
+
+bst = BinarySearchTree()
+bst.insert(15)
+print(bst.find(15))
+bst.insert(10)
+print(bst.find(0))
+print(bst.find(10))
