a690a7a8deaa9c34263646af26226b45d6b493fe
Leandro Tk <leandrodtk@hotmail.com>
2017-07-12 23:47:36 -0300

    Solved black square problem

--- programming_contests/codeforces/div2/black_square.cpp
+++ programming_contests/codeforces/div2/black_square.cpp
@@ -7,41 +7,45 @@
 using namespace std;
 
 int main() {
-  int N, M, product, black = 0, result_square,
+  int N, M, black = 0,
       smaller_black_x = 101,
       smaller_black_y = 101,
-      bigger_black_x = 0,
-      bigger_black_y = 0;
+      bigger_black_x = -1,
+      bigger_black_y = -1,
+      result_square,
+      bigger_side;
 
   vector<string> matrix;
   string line;
   cin >> N >> M;
   cin.ignore();
-  product = N * M;
 
-  if (N == 1 && M > 1 || M == 1 && N > 1) cout << -1 << endl;
-  else {
-    for (int i = 0; i < N; i++) {
-      getline(cin, line);
-      matrix.push_back(line);
-    }
+  for (int i = 0; i < N; i++) {
+    getline(cin, line);
+    matrix.push_back(line);
+  }
 
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (matrix[i][j] == 'B') {
-          black++;
-
-          if (i < smaller_black_y) smaller_black_y = i;
-          if (i > bigger_black_y) bigger_black_y = i;
-          if (j < smaller_black_x) smaller_black_x = j;
-          if (j > bigger_black_x) bigger_black_x = j;
-        }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      if (matrix[i][j] == 'B') {
+        if (i < smaller_black_y) smaller_black_y = i;
+        if (i > bigger_black_y) bigger_black_y = i;
+        if (j < smaller_black_x) smaller_black_x = j;
+        if (j > bigger_black_x) bigger_black_x = j;
+        black++;
       }
     }
-
-    result_square = (bigger_black_x - smaller_black_x + 1) * (bigger_black_y - smaller_black_y + 1);
-    cout << result_square << endl;
   }
+
+  if (black == 0) {
+    cout << 1 << endl;
+    return 0;
+  }
+
+  bigger_side = max(bigger_black_x - smaller_black_x + 1, bigger_black_y - smaller_black_y + 1);
+
+  if (bigger_side > N || bigger_side > M) cout << -1 << endl;
+  else cout << bigger_side * bigger_side - black << endl;
 
   return 0;
 }
