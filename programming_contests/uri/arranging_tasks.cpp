59cf4a51ac974ce1b54d4860db25c0678e31ca0f
Leandro Tk <leandrodtk@hotmail.com>
2017-07-29 00:02:18 -0300

    Trying to solve one more problem

--- programming_contests/uri/arranging_tasks.cpp
+++ programming_contests/uri/arranging_tasks.cpp
@@ -2,35 +2,49 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct task {
+  int V, T;
+};
+
+bool sortByTime(task &ta1, task &ta2) {
+  if (ta1.V >= ta2.V) return true;
+  else if (ta1.V < ta2.V) return false;
+  else return ta1.T <= ta2.T;
+}
+
 int main() {
-	
-	int n1, n2, temp, v, t;
-	
-	while (cin >> n1 >> n2) {
-		while (n1--) {
-			vector< pair<int, vector<int> > > group_of_tasks;
-			cin >> v >> t;
-			int exist_el = -1;
-			
-			vector<int> vec;
-			vec.push_back(v);
-			
-			for (int i = 0; i < group_of_tasks.size(); i++) {
-				if (t == group_of_tasks[i].first) exist_el = i;
-				if (exist_el != -1) group_of_tasks[i].second.push_back(v);
-				else group_of_tasks.push_back(make_pair(t, v));
-			}
-			
-			for (int i = 0; i < group_of_tasks.size(); i++) {
-				cout << group_of_tasks[i].first << " - ";
-				for (int j = 0; j < group_of_tasks[i].second.size(); j++) cout << group_of_tasks[i].second[j] << " ";
-				cout << endl;
-			}
-		}
-	}
-	
-	return 0;
+	int N, H, V, T;
+
+	while (cin >> N >> H) {
+    vector<task> tasks;
+
+    while (N--) {
+      cin >> V >> T;
+      task ta;
+      ta.V = V;
+      ta.T = T;
+      tasks.push_back(ta);
+    }
+
+    sort(tasks.begin(), tasks.end(), sortByTime);
+
+    int value = 0;
+
+    for (int i = 0; i < H && i < 10; i++) {
+      if (tasks[i].T <= H) value += tasks[i].V;
+      cout << "value: " << value << endl;
+    }
+
+    cout << value << endl;
+    cout << "---" << endl;
+    for (int i = 0; i < tasks.size(); i++) {
+      cout << tasks[i].T << " " << tasks[i].V << endl;
+    }
+  }
+
+  return 0;
 }
