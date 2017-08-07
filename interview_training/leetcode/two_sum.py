252ccedd79450721374eb523803f2a42ae59af74
Leandro Tk <leandrodtk@hotmail.com>
2017-08-06 22:54:43 -0300

    Solved first leetcode problem: two sum

---
+++ interview_training/leetcode/two_sum.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/two-sum/description/
+
+def two_sum(self, nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
