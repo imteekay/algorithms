f085bab68d77732b8e236dcc496584bfc959f06f
Leandro Tk <leandrodtk@hotmail.com>
2017-08-12 13:46:36 -0300

    Fixing two sum algorithm method

--- interview_training/leetcode/two_sum.py
+++ interview_training/leetcode/two_sum.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/two-sum/description/
 
-def two_sum(self, nums, target):
+def two_sum(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
