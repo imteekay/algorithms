088394c8853788efa4d2641df4125ed45696dcf0
Leandro Tk <leandrodtk@hotmail.com>
2017-08-12 14:42:59 -0300

    Leetcode: Solved one more problem for my interview training

---
+++ interview_training/leetcode/reverse_integer.py
@@ -0,0 +1,25 @@
+def reverse(x):
+    number_list = []
+
+    if x < 0:
+        negative = True
+        x *= -1
+    else:
+        negative = False
+
+    while x > 0:
+        number_list.append(x % 10)
+        x /= 10
+
+    result_number = 0
+
+    for index, n in enumerate(number_list):
+        result_number += n * pow(10, len(number_list) - 1 - index)
+
+    if negative:
+        result_number *= -1
+
+    if (result_number < 2147483648*-1 or result_number > 2147483647):
+        return 0
+
+    return result_number
