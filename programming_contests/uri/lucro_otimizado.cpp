0935242981867eb5a0ef49438e6d34e7fdd35238
Leandro Tk <leandrodtk@hotmail.com>
2017-09-17 21:07:30 -0300

    Trying to optimize solution

---
+++ programming_contests/uri/lucro_otimizado.cpp
@@ -0,0 +1,40 @@
+// https://www.urionlinejudge.com.br/judge/pt/problems/view/1310
+
+#include <iostream>
+
+using namespace std;
+
+int max(int a, int b) {
+  return (a > b)? a : b;
+}
+
+int main() {
+  int N, C, pricePerDay;
+
+  while (cin >> N >> pricePerDay) {
+    int dp[N+1][N+1] = {};
+
+    for (int i = 1; i <= N; i++) {
+      cin >> C;
+      dp[i][i] = C - pricePerDay;
+    }
+
+    for (int i = 1; i < N+1; i++) {
+      for (int j = i+1; j < N+1; j++) {
+        if (dp[i][j] == 0) dp[i][j] = dp[i][j-1] + dp[j][j];
+      }
+    }
+
+    int greater = 0;
+
+    for (int i = 1; i < N+1; i++) {
+      for (int j = i; j < N+1; j++) {
+        greater = max(dp[i][j], greater);
+      }
+    }
+
+    cout << greater << endl;
+  }
+
+  return 0;
+}
