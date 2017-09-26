34cc43d71a71c1891b8164c4895caa77fad49aa8
Leandro Tk <leandrotk100@gmail.com>
2017-09-25 21:58:31 -0300

    Leetcode: Solved same tree problem

---
+++ interview_training/leetcode/same_tree.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/same-tree/description/
+
+def is_same_tree(p, q):
+    if (p and not q) or (not p and q):
+        return False
+
+    if not p and not q:
+        return True
+
+    return p.val == q.val and is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
