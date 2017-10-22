8addc15c88dedd1d93b0a147cda4879b05aef304
Leandro Tk <leandrotk100@gmail.com>
2017-10-21 22:49:38 -0200

    Started to implement BST remove_node algorithm

--- data_structures/binary_search_tree_without_node/binary_search_tree.py
+++ data_structures/binary_search_tree_without_node/binary_search_tree.py
@@ -22,6 +22,53 @@ class BinarySearchTree:
 
         return value == self.value
 
+    def remove_node(self, value, parent):
+        if value < self.value and self.left_child:
+            return self.left_child.remove_node(value, self)
+        elif value < self.value:
+            return False
+        elif value > self.value and self.right_child:
+            return self.right_child.remove_node(value, self)
+        elif value > self.value:
+            return False
+        else:
+            if self.left_child is None and self.right_child is None and self == parent.left_child:
+                parent.left_child = None
+                self.clear_node()
+                return True
+            elif self.left_child is None and self.right_child is None and self == parent.right_child:
+                parent.right_child = None
+                return True
+            elif self.left_child and self == parent.left_child:
+                parent.left_child = self.left_child
+                self.clear_node()
+                return True
+            elif self.left_child and self == parent.right_child:
+                parent.right_child = self.left_child
+                self.clear_node()
+                return True
+            elif self.right_child and self == parent.left_child:
+                parent.left_child = self.right_child
+                self.clear_node()
+                return True
+            elif self.right_child and self == parent.right_child:
+                parent.right_child = self.right_child
+                self.clear_node()
+                return True
+            else:
+
+
+    def clear_node(self):
+        self.value = None
+        self.left_child = None
+        self.right_child = None
+
+    def find_smaller_node(self):
+        if self.left_child:
+            return self.left_child.find_smaller_node()
+        else:
+            return self
+
     def find_minimum_value(self):
         if self.left_child:
             return self.left_child.find_minimum_value()
