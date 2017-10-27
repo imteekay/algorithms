dc9403ad4efc14caadd397c179ba957da37dcf2f
Leandro Tk <leandrotk100@gmail.com>
2017-10-27 20:35:05 -0200

    Organizing Big O folder

---
+++ big_o/example2.py
@@ -0,0 +1,12 @@
+def print_pairs(array):
+    for i in array:
+        for j in array:
+            print("%d, %d" %(i, j))
+
+print_pairs([1, 2, 3, 4, 5])
+
+# The complexity of the print_pairs function is: O(N^2)
+# We do a nested loop = length(array) ^ 2 = N * 2
+# Printing the pair = 1
+# There are O(N^2) pairs
+# Conclusion: O(N^2 + 1) = O(N^2)
