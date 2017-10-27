41e5c8e3d6c8098081a2947e71cb59b7a4f4184e
Leandro Tk <leandrotk100@gmail.com>
2017-10-27 20:32:39 -0200

    Big O: fixed example 2

--- interview_training/cracking_the_coding_interview/big_o/example2.py
+++ interview_training/cracking_the_coding_interview/big_o/example2.py
@@ -7,5 +7,6 @@ print_pairs([1, 2, 3, 4, 5])
 
 # The complexity of the print_pairs function is: O(N^2)
 # We do a nested loop = length(array) ^ 2 = N * 2
-# Printing sum and product = 1
+# Printing the pair = 1
+# There are O(N^2) pairs
 # Conclusion: O(N^2 + 1) = O(N^2)
