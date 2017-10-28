cb5db3f44591cb6340a8e3518d05e63c95e184cb
Leandro Tk <leandrotk100@gmail.com>
2017-10-28 16:13:38 -0200

    Big O: fixing is prime algorithm

--- interview_training/cracking_the_coding_interview/big_o/example10.py
+++ interview_training/cracking_the_coding_interview/big_o/example10.py
@@ -1,8 +1,14 @@
 from math import sqrt
 
 def is_prime(num):
-    if num <= 2:
+    if num < 2:
+        return False
+
+    if num == 2:
         return True
+
+    if num % 2 == 0:
+        return False
 
     for i in range(3, int(sqrt(num)) + 1, 2):
         if num % i == 0:
@@ -10,8 +16,10 @@ def is_prime(num):
 
     return True
 
-print(is_prime(9))
-print(is_prime(17))
-print(is_prime(24))
+print(is_prime(1)) # False
+print(is_prime(2)) # True
+print(is_prime(9)) # False
+print(is_prime(17)) # True
+print(is_prime(24)) # False
 
 # is prime function has complexity of O(sqrt(N))
