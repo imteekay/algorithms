03c79465bd18eb6c415678bd1bbf57becb80ef2b
Leandro Tk <leandrotk100@gmail.com>
2017-11-12 21:57:32 -0200

    URI Online Judge: Solved sucessor par problem

---
+++ programming_contests/uri/sucessor_par.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long int X;
+    cin >> X;
+    if (X % 2 == 0) cout << X + 2 << endl;
+    else cout << X + 1 << endl;
+    return 0;
+}
