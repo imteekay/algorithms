12cb9df39ff495631e056bb981e4dcefd534567e
Leandro Tk <leandrotk100@gmail.com>
2017-12-24 22:24:11 -0200

    Hacker Rank: solved whats your name string problem

---
+++ competitive_programming/programming_contests/hacker_rank/strings/whats_your_name.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/whats-your-name/problem
+
+# Guido
+# Rossum
+# Hello Guido Rossum! You just delved into python.
+
+def print_full_name(a, b):
+    print "Hello %s %s! You just delved into python." %(a, b)
+
+print_full_name('Guido', 'Rossum')
