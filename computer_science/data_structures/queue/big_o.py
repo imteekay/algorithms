67304095162b99c5249a977da0d0c5052362d2c4
Leandro Tk <leandrotk100@gmail.com>
2018-02-20 22:51:14 -0300

    Big O: queue

---
+++ computer_science/data_structures/queue/big_o.py
@@ -0,0 +1,8 @@
+# Queue Big O complexity
+
+# Enqueue (push): O(1) - Constant Time
+# Dequeue (remove): O(1) - Constant Time
+# Front: O(1) - Constant Time
+# Back: O(1) - Constant Time
+# Is Empty: O(1) - Constant Time
+# Size: O(1) - Constant Time
