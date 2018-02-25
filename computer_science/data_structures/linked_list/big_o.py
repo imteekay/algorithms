0747ccabbfd311a56595440b26037edb5d95ff1e
Leandro Tk <leandrotk100@gmail.com>
2018-02-25 16:11:13 -0300

    Added big o complecity for linked list

---
+++ computer_science/data_structures/linked_list/big_o.py
@@ -0,0 +1,8 @@
+# Linked List Big O complexity
+
+# Push Front: O(1) - Constant Time
+# Push Back: O(N) - Linear Time
+# Remove: O(N) - Linear Time
+# Is Empty: O(1) - Constant Time
+# Size: O(N) - Linear Time
+# Search: O(N) - Linear Time
