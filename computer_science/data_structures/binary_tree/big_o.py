d27dfe1fabff3823383e101df3e93a01bc8f238a
Leandro Tk <leandrotk100@gmail.com>
2018-02-27 22:40:28 -0300

    Added big o complexity for binary tree

---
+++ computer_science/data_structures/binary_tree/big_o.py
@@ -0,0 +1,6 @@
+# Binary Tree: Big O Complexity
+
+# Search: O(log N) - Logarithmic Time
+# Insert: O(log N) - Logarithmic Time
+# Delete: O(log N) - Logarithmic Time
+# Traversal: O(log N) - Logarithmic Time
