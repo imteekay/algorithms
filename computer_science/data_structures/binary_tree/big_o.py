eb4c1561542c171db9db7cfda895622489b60672
Leandro Tk <leandrotk100@gmail.com>
2018-03-07 23:08:47 -0300

    Fixing big o for binary tree

--- computer_science/data_structures/binary_tree/big_o.py
+++ computer_science/data_structures/binary_tree/big_o.py
@@ -3,4 +3,4 @@
 # Search: O(log N) - Logarithmic Time
 # Insert: O(log N) - Logarithmic Time
 # Delete: O(log N) - Logarithmic Time
-# Traversal: O(N) - Logarithmic Time
+# Traversal: O(N) - Linear Time
