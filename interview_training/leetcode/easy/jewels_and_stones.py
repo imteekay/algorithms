b24df58b4150b31ab196233129e0aba0de63a4a4
TK <leandrotk100@gmail.com>
2018-05-22 21:01:39 -0300

    Leetcode: Solve jewels and stones problem

---
+++ interview_training/leetcode/easy/jewels_and_stones.py
@@ -0,0 +1,18 @@
+# input: J = "aA" | S = "aAAbbbb"
+# output: 3
+
+# input: J = "z", S = "ZZ"
+# output: 0
+
+def num_jewels_in_stones(J, S):
+    jewels = 0
+
+    for j in J:
+        for s in S:
+            if s == j:
+                jewels += 1
+
+    return jewels
+
+print(num_jewels_in_stones("aA", "aAAbbbb"))
+print(num_jewels_in_stones("z", "ZZ"))
