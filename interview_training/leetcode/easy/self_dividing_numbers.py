e0fd67d117c9f56af06669bd715afae720d1fbe7
TK <leandrotk100@gmail.com>
2018-06-04 23:32:48 -0300

    Leetcode: self dividing numbers refactoring

--- interview_training/leetcode/easy/self_dividing_numbers.py
+++ interview_training/leetcode/easy/self_dividing_numbers.py
@@ -6,11 +6,7 @@ Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
 '''
 
 def self_dividing_numbers(left, right):
-    output = []
-
-    for number in range(left, right + 1):
-        if dividing(number, number): output.append(number)
-
+    output = [number for number in range(left, right + 1) if dividing(number, number)]
     return output
 
 def dividing(number, original):
