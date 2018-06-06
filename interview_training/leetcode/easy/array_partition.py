f651622a1ddc156243e40ec4dba624390f1435ba
TK <leandrotk100@gmail.com>
2018-06-05 22:50:24 -0300

    Leetcode: refactoring array partition problem to 1 line

--- interview_training/leetcode/easy/array_partition.py
+++ interview_training/leetcode/easy/array_partition.py
@@ -6,5 +6,6 @@ Output: 4
 '''
 
 def array_pair_sum(nums):
-    nums.sort()
-    return sum(nums[i] for i in range(0, len(nums), 2))
+    return sum(sorted(nums)[::2])
+
+print(array_pair_sum([1, 4, 3, 2]))
