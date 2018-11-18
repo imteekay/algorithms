87b2a28d3e9cf99fafa4fd5f28f657ec09dc0b63
TK <leandrotk100@gmail.com>
2018-11-18 21:12:39 -0200

    Pass code through linter

--- interview_training/daily_code_problems/problem_1.py
+++ interview_training/daily_code_problems/problem_1.py
@@ -1,8 +1,8 @@
-'''
+"""
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-'''
+"""
 
 k = 17
 l = [3, 7, 10, 15]
