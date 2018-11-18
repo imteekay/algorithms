2397d8d927933c0c5fff5834e782be860b16c71b
TK <leandrotk100@gmail.com>
2018-11-18 21:22:09 -0200

    Refactor logic to put it inside a function

--- interview_training/daily_code_problems/problem_1.py
+++ interview_training/daily_code_problems/problem_1.py
@@ -4,18 +4,21 @@ Given a list of numbers and a number k, return whether any two numbers from the 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 """
 
-k = 17
-l = [3, 7, 10, 15]
-start, end = 0, len(l) - 1
-found = False
 
-while start < end:
-    if l[start] + l[end] == k:
-        found = True
-        break
-    elif l[start] + l[end] > k:
-        end -= 1
-    else:
-        start += 1
+def found_sum_of_two_numbers(k, l):
+    start, end = 0, len(l) - 1
+    found = False
 
-print(found)
+    while start < end:
+        if l[start] + l[end] == k:
+            found = True
+            break
+        elif l[start] + l[end] > k:
+            end -= 1
+        else:
+            start += 1
+
+    return found
+
+
+print(found_sum_of_two_numbers(17, [3, 7, 10, 15]))
