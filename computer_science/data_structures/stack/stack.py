f8e67a366c2075a760f403f7e236e54f437f07a7
TK <leandrotk100@gmail.com>
2019-11-26 05:47:51 -0300

    Improvements on stack DS

--- computer_science/data_structures/stack/stack.py
+++ computer_science/data_structures/stack/stack.py
@@ -6,6 +6,9 @@ class Stack:
         self.items.append(item)
 
     def pop(self):
+        if self.is_empty():
+            return None
+
         return self.items.pop()
 
     def peek(self):
@@ -16,3 +19,33 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+
+stack = Stack()
+
+print(stack.is_empty())  # True
+
+stack.push(1)
+
+top = stack.peek()
+print(top)  # 1
+
+print(stack.is_empty())  # False
+
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+
+top = stack.peek()
+print(top)  # 5
+
+count = stack.size()
+print(count)  # 5
+
+print(stack.pop())  # 5
+print(stack.pop())  # 4
+print(stack.pop())  # 3
+print(stack.pop())  # 2
+print(stack.pop())  # 1
+print(stack.pop())  # None
