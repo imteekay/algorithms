df3fc2834bc6c96783dd312c4fab10d2c0aca2c4
TK <leandrotk100@gmail.com>
2020-01-25 00:04:37 -0300

    improvement 002

--- interview_training/daily_code_problems/002.py
+++ interview_training/daily_code_problems/002.py
@@ -8,18 +8,19 @@ the expected output would be [120, 60, 40, 30, 24].
 If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 """
 
-
-def product_with_exception(ar):
-    product_of_all = reduce((lambda product, item: product * item), ar)
-    result = []
-
-    for item in ar:
-        product_of_all_with_exception = product_of_all / item
-        result.append(product_of_all_with_exception)
-
-    return result
+from functools import reduce
 
 
-print(product_with_exception([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24])
-print(product_with_exception([3, 2, 1]) == [2, 3, 6])
-print(product_with_exception([-20, 20, 50, 10]) == [10000, -10000, -4000, -20000])
+def product_with_exception(integers):
+    product_of_all = reduce((lambda product, item: product * item), integers)
+    return list(map(lambda item: product_of_all / item, integers))
+
+
+def test_product_with_exception(integers, result):
+    print(product_with_exception(integers) == result)
+
+
+test_product_with_exception([1, 2, 3, 4, 5], [120, 60, 40, 30, 24])
+test_product_with_exception([3, 2, 1], [2, 3, 6])
+test_product_with_exception(
+    [-20, 20, 50, 10], [10000, -10000, -4000, -20000])
