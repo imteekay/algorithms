ecc26e33a6cf21146df053db1a7056b8e920332d
TK <leandrotk100@gmail.com>
2020-01-24 23:30:44 -0300

    Brute force solution and fix optimized solution

--- interview_training/daily_code_problems/001.py
+++ interview_training/daily_code_problems/001.py
@@ -5,20 +5,37 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 """
 
 
-def found_sum_of_two_numbers(k, l):
-    start, end = 0, len(l) - 1
-    found = False
+def found_sum_of_two_numbers_brute_force(k, numbers):
+    for index_1 in range(len(numbers)):
+        for index_2 in range(index_1, len(numbers)):
+            if numbers[index_1] + numbers[index_2] == k:
+                return True
+
+    return False
+
+
+def found_sum_of_two_numbers(k, numbers):
+    start = 0
+    end = len(numbers) - 1
+    sorted_numbers = sorted(numbers)
 
     while start < end:
-        if l[start] + l[end] == k:
-            found = True
-            break
-        elif l[start] + l[end] > k:
+        if sorted_numbers[start] + sorted_numbers[end] == k:
+            return True
+        elif sorted_numbers[start] + sorted_numbers[end] > k:
             end -= 1
         else:
             start += 1
 
-    return found
+    return False
 
 
-print(found_sum_of_two_numbers(17, [3, 7, 10, 15]))
+def test_algorithm(k, numbers):
+    print('Brute force:', found_sum_of_two_numbers_brute_force(k, numbers))
+    print('Optmized:', found_sum_of_two_numbers(k, numbers))
+    print()
+
+
+test_algorithm(17, [10, 15, 3, 7])
+test_algorithm(17, [15, 10, 3, 7])
+test_algorithm(17, [15, 10, 3, 8])
