b4fe6c2612cdcb52dc18b26cef260c7aba4c2fa5
TK <leandrotk100@gmail.com>
2020-03-15 21:42:56 -0300

    Leetcode: solve strStr problem

---
+++ interview_training/cracking_the_coding_interview/string/001.07.py
@@ -0,0 +1,6 @@
+'''
+Rotate Matrix: Given an image represented by an NxN matrix,
+where each pixel in the image is 4 bytes,
+write a method to rotate the image by 90 degrees.
+Can you do this in place?
+'''
---
+++ interview_training/leetcode/easy/str_str/str_str.py
@@ -0,0 +1,42 @@
+'''
+https://leetcode.com/problems/implement-strstr
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack,
+or -1 if needle is not part of haystack.
+
+- Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+- Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+'''
+
+
+def strStr(haystack, needle):
+    if needle == '':
+        return 0
+
+    needle_first_char = needle[0]
+    neddle_length = len(needle)
+
+    for index in range(len(haystack) - neddle_length + 1):
+        if haystack[index] == needle_first_char and haystack[index:index+neddle_length] == needle:
+            return index
+
+    return -1
+
+
+test_data = [
+    ("hello", "ll", 2),
+    ("aaaaa", "bba", -1),
+    ("other", "", 0),
+]
+
+for haystack, needle, expected in test_data:
+    result = strStr(haystack, needle)
+    print('result as expected?', result == expected)
