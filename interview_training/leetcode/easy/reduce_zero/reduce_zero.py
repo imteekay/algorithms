fac5cf470e3fe60d65797c4d5c662d722a8b97d7
TK <leandrotk100@gmail.com>
2020-05-17 22:12:11 -0300

    Solve reduce zero

---
+++ interview_training/leetcode/easy/reduce_zero/reduce_zero.py
@@ -0,0 +1,25 @@
+def reduce_number(num):
+    if num % 2 == 0:
+        return num / 2
+    else:
+        return num - 1
+
+
+def number_of_steps(num):
+    counter = 0
+
+    while num:
+        counter += 1
+        num = reduce_number(num)
+
+    return counter
+
+
+data_tests = [
+    (14, 6),
+    (8, 4)
+]
+
+for number, expected in data_tests:
+    result = number_of_steps(number)
+    print(result, expected == result)
