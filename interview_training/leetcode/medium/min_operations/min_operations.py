1a3ae8999a5ed6c50c6f820190ad3d0649ff0f7f
TK <leandrotk100@gmail.com>
2020-08-26 19:02:29 -0300

    Solve min operations

--- interview_training/leetcode/medium/min_operations/min_operations.py
+++ interview_training/leetcode/medium/min_operations/min_operations.py
@@ -1,16 +1,7 @@
 def min_operations(n):
-    numbers = []
-    sum = 0
-
-    for i in range(n):
-        num = 2 * i + 1
-        numbers.append(num)
-        sum += num
-
-    medium = sum // n
     operations = 0
 
     for index in range(n // 2):
-        operations += (medium - numbers[index])
+        operations += n - (2 * index + 1)
 
     return operations
