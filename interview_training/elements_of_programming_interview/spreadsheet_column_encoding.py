28bd7384dd0a356a74884eee5b072963ad3916aa
= <leandrotk100@gmail.com>
2021-02-22 21:54:36 -0300

    Solve spreadcheet column encoding

---
+++ interview_training/elements_of_programming_interview/spreadsheet_column_encoding.py
@@ -0,0 +1,36 @@
+encoding_mapper = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+    'G': 7,
+    'H': 8,
+    'I': 9,
+    'J': 10,
+    'K': 11,
+    'L': 12,
+    'M': 13,
+    'N': 14,
+    'O': 15,
+    'P': 16,
+    'Q': 17,
+    'R': 18,
+    'S': 19,
+    'T': 20,
+    'U': 21,
+    'V': 22,
+    'W': 23,
+    'X': 24,
+    'Y': 25,
+    'Z': 26,
+}
+
+def spreadcheet_column_encoding(encoding):
+    number, reversed_encoding = 0, encoding[::-1]
+
+    for index in range(len(reversed_encoding)):
+        number += (26 ** index) * (encoding_mapper[reversed_encoding[index]])
+
+    return number
