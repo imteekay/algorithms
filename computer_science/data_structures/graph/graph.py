d73ef26a1e0f0e053515dd7d5415c1516217b363
= <leandrotk100@gmail.com>
2021-02-25 07:32:29 -0300

    Graph

--- computer_science/data_structures/graph/graph.py
+++ computer_science/data_structures/graph/graph.py
@@ -1,16 +1,46 @@
+class Graph:
+    def __init__(self):
+        self.vertices = []
+
+    def add_vertex(self, key):
+        if self.contains(self.vertices, key):
+            print("Vertex", key, "not added because it is an existing key")
+        else:
+            self.vertices.append(Vertex(key))
+
+    def print(self):
+        for vertex in self.vertices:
+            print("Vertex", vertex.key)
+            for neighbor in vertex.adjacent:
+                print(neighbor.key)
+
+    def add_edge(self, from_key, to_key):
+        from_vertex = self.get_vertex(from_key)
+        to_vertex = self.get_vertex(to_key)
+
+        if from_vertex is None or to_vertex is None:
+            print("Invalid edge (", from_key, "-->", to_key, ")")
+            return
+        
+        if self.contains(from_vertex.adjacent, to_key):
+            print("Edge", to_key, "not added because it is already an existing neighbor")
+            return
+
+        from_vertex.adjacent.append(to_vertex)
+
+    def get_vertex(self, key):
+        for vertex in self.vertices:
+            if vertex.key == key:
+                return vertex
+
+    def contains(self, vertices, key):
+        for vertex in vertices:
+            if vertex.key == key:
+                return True
+
+        return False
+
 class Vertex:
     def __init__(self, key):
         self.key = key
-        self.connected_to = {}
-
-    def add_neighbor(self, neighbor, weight):
-        self.connected_to[neighbor] = weight
-
-class Graph:
-    def __init__(self):
-        self.vertices_list = {}
-        self.number_of_vertices = 0
-
-    def add_vertex(self, key):
-        self.vertices_list[key] = Vertex(key)
-        self.number_of_vertices += 1
+        self.adjacent = []
--- computer_science/data_structures/graph/test_graph.py
+++ computer_science/data_structures/graph/test_graph.py
@@ -1,23 +1,16 @@
 from graph import *
 
-graph = Graph()
+g = Graph()
 
-for i in range(6):
-    graph.add_vertex(i)
+for i in range(5):
+    g.add_vertex(i)
 
-print(graph.vertices_list)
-print(graph.number_of_vertices)
-
-graph.vertices_list[0].add_neighbor(graph.vertices_list[1], 5)
-graph.vertices_list[0].add_neighbor(graph.vertices_list[5], 2)
-graph.vertices_list[1].add_neighbor(graph.vertices_list[2], 4)
-graph.vertices_list[2].add_neighbor(graph.vertices_list[3], 9)
-graph.vertices_list[3].add_neighbor(graph.vertices_list[4], 7)
-graph.vertices_list[3].add_neighbor(graph.vertices_list[5], 3)
-graph.vertices_list[4].add_neighbor(graph.vertices_list[0], 1)
-graph.vertices_list[5].add_neighbor(graph.vertices_list[4], 8)
-graph.vertices_list[5].add_neighbor(graph.vertices_list[2], 1)
-
-for vertex in graph.vertices_list.values():
-    for neighbor in vertex.connected_to.keys():
-        print("(%s %s)" %(vertex.key, neighbor.key))
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 3)
+g.add_edge(2, 4)
+g.add_edge(1, 2)
+g.add_edge(6, 2)
+g.add_edge(3, 2)
+g.add_edge(1, 1)
+g.print()
