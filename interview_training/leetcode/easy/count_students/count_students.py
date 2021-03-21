13ad9cc10f4aad57d092155cff0fb4ba57917ebe
= <leandrotk100@gmail.com>
2021-03-21 15:29:44 -0300

    Solve count students problem

---
+++ interview_training/leetcode/easy/count_students/count_students.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/number-of-students-unable-to-eat-lunch
+
+def is_empty(arr):
+    return len(arr) == 0
+    
+def count_students(students, sandwiches):
+    preferred_circles = 0
+    preferred_squares = 0
+    
+    for student in students:
+        if student:
+            preferred_squares += 1
+        else:
+            preferred_circles += 1
+            
+    while not is_empty(students) and not is_empty(sandwiches):
+        if students[0] == sandwiches[0]:
+            if students[0]:
+                preferred_squares -= 1
+            else:
+                preferred_circles -= 1
+            
+            del students[0]
+            del sandwiches[0]
+        else:
+            students.append(students[0])
+            del students[0]
+            
+        if not is_empty(sandwiches) and (sandwiches[0] and preferred_squares == 0 or sandwiches[0] == 0 and preferred_circles == 0):
+            break
+            
+    return len(students)
