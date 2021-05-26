0ee9deacdd132d18737b53a7fce49ec8120c3535
= <leandrotk100@gmail.com>
2021-05-26 08:59:02 -0300

    CTCI: Solve is_unique with dict

---
+++ interview_training/cracking_the_coding_interview/technical_questions/chapter_01/is_unique.py
@@ -0,0 +1,56 @@
+import time
+import unittest
+from collections import defaultdict
+
+
+def is_unique_chars_using_dictionary(text):
+    char_counter = {}
+
+    for char in text:
+        if char in char_counter:
+            return False
+        else:
+            char_counter[char] = 1
+    
+    return True
+
+
+
+class Test(unittest.TestCase):
+    test_cases = [
+        ("abcd", True),
+        ("s4fad", True),
+        ("", True),
+        ("23ds2", False),
+        ("hb 627jh=j ()", False),
+        ("".join([chr(val) for val in range(128)]), True),  # unique 128 chars
+        ("".join([chr(val // 2) for val in range(129)]), False),  # non-unique 129 chars
+    ]
+
+    test_functions = [
+        is_unique_chars_using_dictionary,
+        # is_unique_chars_sorting,
+    ]
+
+    def test_is_unique_chars(self):
+        num_runs = 1000
+        function_runtimes = defaultdict(float)
+
+        for _ in range(num_runs):
+            for text, expected in self.test_cases:
+                for is_unique_chars in self.test_functions:
+                    start = time.perf_counter()
+                    assert (
+                        is_unique_chars(text) == expected
+                    ), f"{is_unique_chars.__name__} failed for value: {text}"
+                    function_runtimes[is_unique_chars.__name__] += (
+                        time.perf_counter() - start
+                    ) * 1000
+
+        print(f"\n{num_runs} runs")
+        for function_name, runtime in function_runtimes.items():
+            print(f"{function_name}: {runtime:.1f}ms")
+
+
+if __name__ == "__main__":
+    unittest.main()
