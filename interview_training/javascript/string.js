fcf1bccd2a06bd62becb50e027d2051486e85c99
= <leandrotk100@gmail.com>
2021-07-04 15:42:32 -0300

    JS strings

---
+++ interview_training/javascript/string.js
@@ -0,0 +1,13 @@
+const string = 'nihongo';
+
+string.replace('n', 'N'); // 'Nihongo'
+
+string.replaceAll('n', 'N'); // 'NihoNgo'
+
+string.slice(2, 4); // 'ho'
+
+string.toUpperCase(); // 'NIHONGO'
+
+string.toUpperCase().toLowerCase(); // 'nihongo'
+
+'  nihongo   '.trim(); // 'nihongo'
