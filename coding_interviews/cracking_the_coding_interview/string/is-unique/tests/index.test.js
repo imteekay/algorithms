9fa9ecb13b7bbd573f65a40f1f3329d02d54b787
TK <leandrotk100@gmail.com>
2022-02-07 09:15:28 -0300

    is unique: string characters

---
+++ coding_interviews/cracking_the_coding_interview/string/is-unique/tests/index.test.js
@@ -0,0 +1,32 @@
+import { describe, expect, it } from 'vitest';
+import { isUnique, isUniqueWithSet, isUniqueWithSetSimplified } from '../index';
+
+describe('isUnique', () => {
+  it('returns true for unique characters', () => {
+    expect(isUnique('asdfghjkl')).toBeTruthy();
+  });
+
+  it('returns false for non-unique characters', () => {
+    expect(isUnique('asdfghjkla')).toBeFalsy();
+  });
+});
+
+describe('isUniqueWithSet', () => {
+  it('returns true for unique characters', () => {
+    expect(isUniqueWithSet('asdfghjkl')).toBeTruthy();
+  });
+
+  it('returns false for non-unique characters', () => {
+    expect(isUniqueWithSet('asdfghjkla')).toBeFalsy();
+  });
+});
+
+describe('isUniqueWithSetSimplified', () => {
+  it('returns true for unique characters', () => {
+    expect(isUniqueWithSetSimplified('asdfghjkl')).toBeTruthy();
+  });
+
+  it('returns false for non-unique characters', () => {
+    expect(isUniqueWithSetSimplified('asdfghjkla')).toBeFalsy();
+  });
+});
