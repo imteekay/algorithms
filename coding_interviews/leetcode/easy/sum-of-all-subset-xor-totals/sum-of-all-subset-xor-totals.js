8382399d43630ad1f39c304fc711fe4fad028c82
TK <leandrotk100@gmail.com>
2022-05-11 16:17:49 -0300

    remove logs

--- coding_interviews/leetcode/easy/sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.js
+++ coding_interviews/leetcode/easy/sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.js
@@ -1,14 +1,11 @@
 function getAllSubsets(subset, nums, output, index) {
   if (index == nums.length) {
     subset.push(output);
-    console.log('subset', subset);
     return;
   }
 
-  console.log('output', output);
   getAllSubsets(subset, nums, [...output], index + 1);
   output.push(nums[index]);
-  console.log('output', output);
   getAllSubsets(subset, nums, [...output], index + 1);
 }
 
@@ -27,7 +24,6 @@ function subsetXORSum(nums) {
   let subset = [];
 
   getAllSubsets(subset, nums, [], 0);
-  console.log('subset', subset);
 
   for (let list of subset) {
     if (list.length) total += calculateTotalXOR(list);
