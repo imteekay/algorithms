b655087fc8ed11ac206fa183a6b8f11202543d40
TK <leandrotk100@gmail.com>
2022-05-22 11:50:04 -0300

    add simpler to default

--- coding_interviews/leetcode/easy/reverse-linked-list/reverse-linked-list.js
+++ coding_interviews/leetcode/easy/reverse-linked-list/reverse-linked-list.js
@@ -19,3 +19,18 @@ function reverseList(head) {
 
   return firstNode;
 }
+
+function reverseListSimpler(head) {
+  let previous = null;
+  let current = head;
+  let next;
+
+  while (current) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  return previous;
+}
