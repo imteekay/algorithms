a3d789c6c026f21de7877c4ac4f8f6a4dbfbde0b
TK <leandrotk100@gmail.com>
2022-05-30 14:22:42 -0300

    Add slice examples

---
+++ coding_interviews/javascript/array/slice.js
@@ -0,0 +1,5 @@
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+list.slice(0); // all elements: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list.slice(0, 3); // first 3 elements: [1, 2, 3]
+list.slice(1, 4); // from 2 to 4: [2, 3, 4]
