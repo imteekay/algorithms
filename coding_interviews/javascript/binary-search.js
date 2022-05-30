aa455e43305d1d5ebc01bb1a3b614f1a0e796fe2
TK <leandrotk100@gmail.com>
2022-05-30 14:14:38 -0300

    binary search

--- coding_interviews/javascript/binary-search.js
+++ coding_interviews/javascript/binary-search.js
@@ -1,15 +1,16 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const target = 2;
+function getMiddle(start, end) {
+  return Math.floor((start + end) / 2);
+}
 
-const binarySearch = (numbers, target) => {
+function binarySearch(numbers, target) {
   let start = 0;
   let end = numbers.length - 1;
-  let middle = Math.floor(numbers.length / 2);
+  let middle = getMiddle(start, end);
   let found = false;
 
   while (start <= end && !found) {
+    middle = getMiddle(start, end);
     const middleNumber = numbers[middle];
-    middle = Math.floor((start + end) / 2);
 
     if (middleNumber === target) found = true;
     if (middleNumber > target) end = middle - 1;
@@ -17,6 +18,15 @@ const binarySearch = (numbers, target) => {
   }
 
   return found;
-};
+}
 
-binarySearch(numbers, target);
+function logResult(list, target) {
+  console.log(binarySearch(list, target));
+}
+
+logResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5); // true
+logResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11); // false
+logResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -1); // false
+logResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0); // false
+logResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9); // true
+logResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1); // true
