53dbb919e705a66acb6c8f70c4b14f265a8766c6
TK <leandrotk100@gmail.com>
2022-06-04 21:55:39 -0300

    add explanation

--- coding_interviews/leetcode/medium/clone-graph/clone-graph.js
+++ coding_interviews/leetcode/medium/clone-graph/clone-graph.js
@@ -1,3 +1,11 @@
+/*
+Cloning a graph using a Breadth-First Search
+— build a queue of all nodes in the graph
+— build a hashmap of all cloned nodes
+— if it's on the map: it's counted as visited & add to the neighbors' list 
+— else create from scratch
+*/
+
 function cloneGraph(node) {
   if (!node) return;
 
