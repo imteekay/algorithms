52ab40a9e060d94200654439b0c2ddf15c66e5ba
TK <leandrotk100@gmail.com>
2022-06-11 18:54:17 -0300

    update

--- coding_interviews/leetcode/medium/kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.js
+++ coding_interviews/leetcode/medium/kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.js
@@ -1,3 +1,6 @@
+// Solution 1
+// additional memory usage: array
+
 function traverse(node, list) {
   return node
     ? [
@@ -11,4 +14,31 @@ function traverse(node, list) {
 
 function kthSmallest(root, k) {
   return traverse(root, [])[k - 1];
+}
+
+// =========================== // ===========================
+
+// Solution 2
+// Less memory
+let counter;
+let nodeValue;
+
+function traverseAndGetKthSmallest(node, k) {
+  if (!node) return;
+
+  traverseAndGetKthSmallest(node.left, k);
+  counter++;
+
+  if (counter === k) {
+    nodeValue = node.val;
+  }
+
+  traverseAndGetKthSmallest(node.right, k);
+}
+
+function kthSmallest(root, k) {
+  counter = 0;
+  nodeValue = -1;
+  traverseAndGetKthSmallest(root, k);
+  return nodeValue;
 }
