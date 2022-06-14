569ed68eba3eeff902f8078992099c28ce4d7cd6
TK <leandrotk100@gmail.com>
2022-06-14 00:18:21 -0300

    add binary search solution

--- coding_interviews/top-problems/first-and-last-index.js
+++ coding_interviews/top-problems/first-and-last-index.js
@@ -2,10 +2,10 @@
 Given a sorted array of integers `arr` and an integer target,
 find the index of the first and last position of target in `arr`.
 In target can't be found in `arr`, return [-1, -1]
-
-Runtime: O(N)
-Space: O(1)
 */
+
+// Runtime: O(N)
+// Space: O(1)
 
 export function firstAndLastIndex(list, target) {
   let first = -1;
@@ -26,4 +26,58 @@ export function firstAndLastIndex(list, target) {
   }
 
   return [first, last];
+}
+
+// Runtime: O(logN)
+// Space: O(1)
+
+function getMiddle(start, end) {
+  return Math.floor((start + end) / 2);
+}
+
+function findIndex(list, target, type) {
+  let start = 0;
+  let end = list.length - 1;
+  let middle = getMiddle(start, end);
+  let index = -1;
+
+  while (start <= end) {
+    if (target > list[middle]) {
+      start = middle + 1;
+      middle = getMiddle(start, end);
+    }
+
+    if (target < list[middle]) {
+      end = middle - 1;
+      middle = getMiddle(start, end);
+    }
+
+    if (target === list[middle]) {
+      index = middle;
+
+      if (type === 'findFirst') {
+        end = middle - 1;
+      }
+
+      if (type === 'findLast') {
+        start = middle + 1;
+      }
+
+      middle = getMiddle(start, end);
+    }
+  }
+
+  return index;
+}
+
+function findFirstIndex(list, target) {
+  return findIndex(list, target, 'findFirst');
+}
+
+function findLastIndex(list, target) {
+  return findIndex(list, target, 'findLast');
+}
+
+export function firstAndLastIndexBinarySearch(list, target) {
+  return [findFirstIndex(list, target), findLastIndex(list, target)];
 }
--- coding_interviews/top-problems/tests/first-and-last-index.test.js
+++ coding_interviews/top-problems/tests/first-and-last-index.test.js
@@ -1,5 +1,8 @@
 import { describe, expect, it } from 'vitest';
-import { firstAndLastIndex } from '../first-and-last-index';
+import {
+  firstAndLastIndex,
+  firstAndLastIndexBinarySearch,
+} from '../first-and-last-index';
 
 describe('firstAndLastIndex', () => {
   it('', () => {
@@ -18,5 +21,25 @@ describe('firstAndLastIndex', () => {
     expect(firstAndLastIndex([1, 1, 2, 3, 4, 5, 5, 5, 6, 8, 9], 7)).toEqual([
       -1, -1,
     ]);
+  });
+});
+
+describe('firstAndLastIndexBinarySearch', () => {
+  it('', () => {
+    expect(
+      firstAndLastIndexBinarySearch([1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9], 5)
+    ).toEqual([5, 7]);
+  });
+
+  it('', () => {
+    expect(
+      firstAndLastIndexBinarySearch([1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9], 4)
+    ).toEqual([4, 4]);
+  });
+
+  it('', () => {
+    expect(
+      firstAndLastIndexBinarySearch([1, 1, 2, 3, 4, 5, 5, 5, 6, 8, 9], 7)
+    ).toEqual([-1, -1]);
   });
 });
