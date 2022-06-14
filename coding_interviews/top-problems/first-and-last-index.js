7683bc0ce9005d48d7826a75e50d2807afad65a6
TK <leandrotk100@gmail.com>
2022-06-13 23:57:14 -0300

    add complexity

--- coding_interviews/top-problems/first-and-last-index.js
+++ coding_interviews/top-problems/first-and-last-index.js
@@ -2,6 +2,9 @@
 Given a sorted array of integers `arr` and an integer target,
 find the index of the first and last position of target in `arr`.
 In target can't be found in `arr`, return [-1, -1]
+
+Runtime: O(N)
+Space: O(1)
 */
 
 export function firstAndLastIndex(list, target) {
