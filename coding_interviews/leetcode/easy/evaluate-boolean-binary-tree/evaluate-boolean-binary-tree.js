5f279b7f3f5031381baf121e0c14082a7b800154
imteekay <leandrotk100@gmail.com>
2022-07-17 15:10:46 -0300

    improve

--- coding_interviews/leetcode/easy/evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.js
+++ coding_interviews/leetcode/easy/evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.js
@@ -1,17 +1,11 @@
 const values = {
-  false: 0,
-  true: 1,
   or: 2,
   and: 3,
 };
 
 function evaluateTree(root) {
-  // leaf node
-  if ([0, 1].includes(root.val)) {
-    return Boolean(root.val);
-  }
+  if ([0, 1].includes(root.val)) return Boolean(root.val);
 
-  // non-leaf node
   const left = evaluateTree(root.left);
   const right = evaluateTree(root.right);
 
