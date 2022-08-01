225c51d4f5414f6a5404f4133cafe4c6b4c2806f
TK <leandrotk100@gmail.com>
2022-08-01 10:58:37 -0300

    solve next greater element - optimized version

---
+++ coding_interviews/leetcode/easy/next-greater-element-i/next-greater-element-i-optimized.js
@@ -0,0 +1,36 @@
+function peek(stack) {
+  return stack[stack.length - 1];
+}
+
+function isEmpty(stack) {
+  return stack.length === 0;
+}
+
+export function nextGreaterElement(nums1, nums2) {
+  const stack = [];
+  const hashmap = new Map();
+
+  for (num of nums2) {
+    while (!isEmpty(stack) && num > peek(stack)) {
+      hashmap.set(stack.pop(), num);
+    }
+
+    stack.push(num);
+  }
+
+  while (!isEmpty(stack)) {
+    hashmap.set(stack.pop(), -1);
+  }
+
+  const result = [];
+
+  for (let num of nums1) {
+    if (hashmap.has(num)) {
+      result.push(hashmap.get(num));
+    } else {
+      result.push(-1);
+    }
+  }
+
+  return result;
+}
---
+++ coding_interviews/leetcode/easy/next-greater-element-i/tests/next-greater-element-i-optimized.test.js
@@ -0,0 +1,32 @@
+import { describe, expect, it } from 'vitest';
+import { nextGreaterElement } from '../next-greater-element-i-optimized';
+
+describe('nextGreaterElement', () => {
+  it('', () => {
+    expect(nextGreaterElement([4, 1, 2], [1, 3, 4, 2])).toEqual([-1, 3, -1]);
+  });
+
+  it('', () => {
+    expect(nextGreaterElement([2, 4], [1, 2, 3, 4])).toEqual([3, -1]);
+  });
+
+  it('', () => {
+    expect(nextGreaterElement([4, 1, 2], [1, 3, 4, 2, 5])).toEqual([5, 3, 5]);
+  });
+
+  it('', () => {
+    expect(nextGreaterElement([4, 1, 2], [3, 4, 5, 2, 1])).toEqual([5, -1, -1]);
+  });
+
+  it('', () => {
+    expect(nextGreaterElement([4, 1, 2], [3, 4, 5, 2, 1, 6])).toEqual([
+      5, 6, 6,
+    ]);
+  });
+
+  it('', () => {
+    expect(
+      nextGreaterElement([9, 8, 7, 3, 2, 1, 6], [9, 8, 7, 3, 2, 1, 6])
+    ).toEqual([-1, -1, -1, 6, 6, 6, -1]);
+  });
+});
