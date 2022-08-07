0c52278e91c614b5fb451ec0efa130cb95a4b779
TK <leandrotk100@gmail.com>
2022-08-07 09:50:34 -0300

    solve array zero

---
+++ coding_interviews/leetcode/easy/make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.js
@@ -0,0 +1,40 @@
+function smallestExceptZeros(smallest, num) {
+  return smallest ? Math.min(smallest, num) : num;
+}
+
+function getSmallest(nums) {
+  let smallest = 0;
+
+  for (let num of nums) {
+    if (num > 0) {
+      smallest = smallestExceptZeros(smallest, num);
+    }
+  }
+
+  return smallest;
+}
+
+function performSubtraction(nums, smallestToSubtract, smallest) {
+  for (let index = 0; index < nums.length; index++) {
+    let subtractionResult = nums[index] - smallestToSubtract;
+    nums[index] = subtractionResult > 0 ? subtractionResult : 0;
+
+    if (subtractionResult > 0) {
+      smallest = smallestExceptZeros(smallest, subtractionResult);
+    }
+  }
+
+  return smallest;
+}
+
+function minimumOperations(nums) {
+  let count = 0;
+  let smallest = getSmallest(nums);
+
+  while (smallest) {
+    smallest = performSubtraction(nums, smallest, 0);
+    count++;
+  }
+
+  return count;
+}
