b2bb8c0aa8e1108cbc976037371393bba740c45a
TK <leandrotk100@gmail.com>
2022-09-30 15:31:16 -0300

    big o in linked list

---
+++ computer_science/data_structures/linked_list/big-o.js
@@ -0,0 +1,14 @@
+/***
+=== Linked List Big O complexity ===
+
+Push Front: O(1) - Constant Time
+Push Back: O(N) - Linear Time
+  If it has the tail: O(1) - Constant Time
+Push in the middle: O(N) - Linear Time
+Remove: O(N) - Linear Time
+  Remove of head or tail: O(1) - Constant Time
+Is Empty: O(1) - Constant Time
+Size: O(N) - Linear Time
+Search: O(N) - Linear Time
+  Get head or tail: O(1) - Constant Time
+***/
