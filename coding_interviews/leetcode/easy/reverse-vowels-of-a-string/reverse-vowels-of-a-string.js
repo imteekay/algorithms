9d155d4fe266c9dae28532c5ed99aae533060bc6
TK <leandrotk100@gmail.com>
2022-11-04 15:07:06 -0300

    *

---
+++ coding_interviews/leetcode/easy/reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -0,0 +1,25 @@
+function reverseVowels(s) {
+  const allVowels = [];
+  const vowels = 'aeiouAEIOU';
+
+  for (let char of s) {
+    if (vowels.includes(char)) allVowels.push(char);
+  }
+
+  const reversedVowels = [];
+
+  for (
+    let index = 0, vowelIndex = allVowels.length - 1;
+    index < s.length;
+    index++
+  ) {
+    if (vowels.includes(s[index])) {
+      reversedVowels.push(allVowels[vowelIndex]);
+      vowelIndex--;
+    } else {
+      reversedVowels.push(s[index]);
+    }
+  }
+
+  return reversedVowels.join('');
+}
