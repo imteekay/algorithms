3c10ccbf6f703f544a173d84de206526e3977eb8
TK <leandrotk100@gmail.com>
2023-03-12 15:36:57 -0300

    *

--- computer_science/data_structures/binary_tree/BinaryTree/index.js
+++ computer_science/data_structures/binary_tree/BinaryTree/index.js
@@ -4,4 +4,24 @@ export class BinaryTree {
     this.left = null;
     this.right = null;
   }
+
+  insertLeft(value) {
+    if (this.left) {
+      const node = new BinaryTree(value);
+      node.left = this.left;
+      this.left = node;
+    } else {
+      this.left = new BinaryTree(value);
+    }
+  }
+
+  insertRight(value) {
+    if (this.right) {
+      const node = new BinaryTree(value);
+      node.right = this.right;
+      this.right = node;
+    } else {
+      this.right = new BinaryTree(value);
+    }
+  }
 }
--- computer_science/data_structures/binary_tree/BinaryTree/index.test.js
+++ computer_science/data_structures/binary_tree/BinaryTree/index.test.js
@@ -8,4 +8,36 @@ describe('BinaryTree', () => {
     expect(tree.right).toEqual(null);
     expect(tree.left).toEqual(null);
   });
+
+  describe('inserts a left node', () => {
+    it('with a left child', () => {
+      const tree = new BinaryTree('a');
+      tree.left = new BinaryTree('b');
+      tree.insertLeft('c');
+      expect(tree.left.value).toEqual('c');
+      expect(tree.left.left.value).toEqual('b');
+    });
+
+    it('without a left child', () => {
+      const tree = new BinaryTree('a');
+      tree.insertLeft('b');
+      expect(tree.left.value).toEqual('b');
+    });
+  });
+
+  describe('inserts a right node', () => {
+    it('with a right child', () => {
+      const tree = new BinaryTree('a');
+      tree.right = new BinaryTree('b');
+      tree.insertRight('c');
+      expect(tree.right.value).toEqual('c');
+      expect(tree.right.right.value).toEqual('b');
+    });
+
+    it('without a right child', () => {
+      const tree = new BinaryTree('a');
+      tree.insertRight('b');
+      expect(tree.right.value).toEqual('b');
+    });
+  });
 });
