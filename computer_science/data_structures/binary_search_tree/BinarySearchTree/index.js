268e36dc2c812069515c13e257018d93e36afc44
TK <leandrotk100@gmail.com>
2023-03-12 22:45:25 -0300

    *

--- computer_science/data_structures/binary_search_tree/BinarySearchTree/index.js
+++ computer_science/data_structures/binary_search_tree/BinarySearchTree/index.js
@@ -28,4 +28,68 @@ export class BinarySearchTree {
 
     return this.value === value;
   }
+
+  removeNode(value, parent) {
+    if (value < this.value && this.left) {
+      return this.left.removeNode(value, this);
+    }
+
+    if (value < this.value) {
+      return false;
+    }
+
+    if (value > this.value && this.right) {
+      return this.right.removeNode(value, this);
+    }
+
+    if (value > this.value) {
+      return false;
+    }
+
+    if (this.left === null && this.right === null && this == parent.left) {
+      parent.left = null;
+      this.clearNode();
+      return true;
+    }
+
+    if (this.left === null && this.right === null && this == parent.right) {
+      parent.right = null;
+      this.clearNode();
+      return true;
+    }
+
+    if (this.left && this.right === null && this == parent.left) {
+      parent.left = this.left;
+      this.clearNode();
+      return true;
+    }
+
+    if (this.left && this.right === null && this == parent.right) {
+      parent.right = this.left;
+      this.clearNode();
+      return true;
+    }
+
+    if (this.right && this.left === null && this == parent.left) {
+      parent.left = this.right;
+      this.clearNode();
+      return true;
+    }
+
+    if (this.right && this.left === null && this == parent.right) {
+      parent.right = this.right;
+      this.clearNode();
+      return true;
+    }
+
+    this.value = this.right.findMinimumValue();
+    this.right.removeNode(this.value, this);
+    return true;
+  }
+
+  clearNode() {
+    this.value = null;
+    this.left = null;
+    this.right = null;
+  }
 }
--- computer_science/data_structures/binary_search_tree/BinarySearchTree/index.test.js
+++ computer_science/data_structures/binary_search_tree/BinarySearchTree/index.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { BinarySearchTree } from '.';
 
-describe('BinarySearchTree', () => {
+function buildBST() {
   const tree = new BinarySearchTree(50);
 
   tree.insertNode(21);
@@ -12,7 +12,13 @@ describe('BinarySearchTree', () => {
   tree.insertNode(52);
   tree.insertNode(100);
 
+  return tree;
+}
+
+describe('BinarySearchTree', () => {
   it('inserts nodes to the BST', () => {
+    const tree = buildBST();
+
     expect(tree.value).toEqual(50);
     expect(tree.left.value).toEqual(21);
     expect(tree.left.left.value).toEqual(4);
@@ -25,6 +31,8 @@ describe('BinarySearchTree', () => {
 
   describe('findNode', () => {
     it('finds the node in the tree', () => {
+      const tree = buildBST();
+
       expect(tree.findNode(50)).toEqual(true);
       expect(tree.findNode(21)).toEqual(true);
       expect(tree.findNode(4)).toEqual(true);
@@ -35,6 +43,22 @@ describe('BinarySearchTree', () => {
       expect(tree.findNode(100)).toEqual(true);
       expect(tree.findNode(0)).toEqual(false);
       expect(tree.findNode(999)).toEqual(false);
+    });
+  });
+
+  describe('removeNode', () => {
+    it('removes a node in the tree', () => {
+      const tree = buildBST();
+
+      expect(tree.findNode(4)).toEqual(true);
+      expect(tree.removeNode(4, tree)).toEqual(true);
+      expect(tree.findNode(4)).toEqual(false);
+    });
+
+    it('removes a node in the tree', () => {
+      const tree = buildBST();
+
+      expect(tree.findNode(4)).toEqual(true);
     });
   });
 });
