3d920670bf919b2981dc5c3b34c650b6b323d4db
TK <leandrotk100@gmail.com>
2023-03-18 09:15:29 -0300

    *

--- coding_interviews/leetcode/medium/balance-a-binary-search-tree/balance-a-binary-search-tree.js
+++ coding_interviews/leetcode/medium/balance-a-binary-search-tree/balance-a-binary-search-tree.js
@@ -1,49 +1,23 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-
-function getAllNodes(root, nodes) {
+function inorder(root, nodes) {
+  if (root.left) inorder(root.left, nodes);
   nodes.push(root.val);
-  if (root.left) getAllNodes(root.left, nodes);
-  if (root.right) getAllNodes(root.right, nodes);
+  if (root.right) inorder(root.right, nodes);
 }
 
-function sortASC(values) {
-  values.sort((a, b) => a - b);
-}
+function toBST(start, end, nodes) {
+  if (start > end) return null;
 
-function insert(node, value) {
-  if (node.left && value <= node.val) insert(node.left, value);
-  else if (value <= node.val) node.left = new TreeNode(value);
-  else if (node.right && value > node.val) insert(node.right, value);
-  else if (value > node.val) node.right = new TreeNode(value);
-}
+  const middle = Math.floor((start + end) / 2);
+  const bst = new TreeNode(nodes[middle]);
 
-function insertAll(root, values) {
-  for (let value of values) {
-    insert(root, value);
-  }
+  bst.left = toBST(start, middle - 1, nodes);
+  bst.right = toBST(middle + 1, end, nodes);
+
+  return bst;
 }
 
 function balanceBST(root) {
   const nodes = [];
-
-  getAllNodes(root, nodes);
-  sortASC(nodes);
-
-  const middleIndex = Math.floor(nodes.length / 2);
-  const balancedBST = new TreeNode(nodes[middleIndex]);
-  const children = [
-    ...nodes.slice(0, middleIndex),
-    ...nodes.slice(middleIndex + 1),
-  ];
-
-  insertAll(balancedBST, children);
-
-  return balancedBST;
+  inorder(root, nodes);
+  return toBST(0, nodes.length - 1, nodes);
 }
