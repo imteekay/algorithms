066271d3b55de7f921ddf33124278d325f1396ff
TK <leandrotk100@gmail.com>
2023-04-01 14:57:47 -0300

    *

--- coding_interviews/leetcode/easy/minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.js
+++ coding_interviews/leetcode/easy/minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.js
@@ -6,3 +6,30 @@ function getMinimumDifference(root, rootVal = Infinity) {
     getMinimumDifference(root.right, root.val)
   );
 }
+
+// ===============================================================
+
+function traverse(node, list) {
+  if (!node) return null;
+  traverse(node.left, list);
+  list.push(node.val);
+  traverse(node.right, list);
+}
+
+function diff(list) {
+  let minDiff = Infinity;
+  let previous = list[0];
+
+  for (let index = 1; index < list.length; index++) {
+    minDiff = Math.min(list[index] - previous, minDiff);
+    previous = list[index];
+  }
+
+  return minDiff;
+}
+
+function getMinimumDifference(root) {
+  let list = [];
+  traverse(root, list);
+  return diff(list);
+}
