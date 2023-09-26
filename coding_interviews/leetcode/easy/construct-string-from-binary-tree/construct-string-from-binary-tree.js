664ad505c40bb08589295e641a9755982ca8a35a
TK <leandrotk100@gmail.com>
2023-09-26 07:09:33 -0300

    *

--- coding_interviews/leetcode/easy/construct-string-from-binary-tree/construct-string-from-binary-tree.js
+++ coding_interviews/leetcode/easy/construct-string-from-binary-tree/construct-string-from-binary-tree.js
@@ -8,6 +8,10 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+// Time complexity : O(n). The preorder traversal is done over the n nodes of the given Binary Tree.
+// Space complexity : O(n). The depth of the recursion tree can go upto n in case of a skewed tree.
+
 function tree2str(root) {
   if (!root) return '';
 
