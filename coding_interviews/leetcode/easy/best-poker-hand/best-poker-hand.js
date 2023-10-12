12faec2f9a23f1e9a90bb8ad2572a8f1c35a0c1b
TK <leandrotk100@gmail.com>
2023-10-12 15:19:41 -0300

    *

---
+++ coding_interviews/leetcode/easy/best-poker-hand/best-poker-hand.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/best-poker-hand
+
+function buildMap(list) {
+  let map = new Map();
+
+  for (let item of list) {
+    if (map.has(item)) map.set(item, map.get(item) + 1);
+    else map.set(item, 1);
+  }
+
+  return map;
+}
+
+function bestHand(ranks, suits) {
+  let suitsMap = buildMap(suits);
+
+  for (let [card, count] of suitsMap.entries()) {
+    if (count >= 5) return 'Flush';
+  }
+
+  let ranksMap = buildMap(ranks);
+  let hasPair = false;
+
+  for (let [card, count] of ranksMap.entries()) {
+    if (count >= 3) return 'Three of a Kind';
+    if (count >= 2) hasPair = true;
+  }
+
+  return hasPair ? 'Pair' : 'High Card';
+}
