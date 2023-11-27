a6246fae11180149195df7576846680a39adbd67
TK <leandrotk100@gmail.com>
2023-11-27 19:16:00 -0300

    *

---
+++ coding_interviews/algoexpert/two-number-sum/two-number-sum-two-pointers.js
@@ -0,0 +1,23 @@
+function twoNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    let leftNumber = array[left];
+    let rightNumber = array[right];
+
+    if (leftNumber + rightNumber === targetSum) {
+      return [leftNumber, rightNumber];
+    }
+
+    if (leftNumber + rightNumber > targetSum) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return [];
+}
