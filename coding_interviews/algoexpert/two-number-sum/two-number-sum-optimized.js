7b21f5e268258614c1801f12fc6aaeb664719d15
TK <leandrotk100@gmail.com>
2023-11-27 18:40:39 -0300

    *

---
+++ coding_interviews/algoexpert/two-number-sum/two-number-sum-optimized.js
@@ -0,0 +1,15 @@
+function twoNumberSum(array, targetSum) {
+  let hashmap = new Map();
+
+  for (let num of array) {
+    let complement = targetSum - num;
+
+    if (hashmap.has(complement)) {
+      return [num, complement];
+    }
+
+    hashmap.set(num, num);
+  }
+
+  return [];
+}
