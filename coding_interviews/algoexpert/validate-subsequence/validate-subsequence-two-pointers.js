87acad383982069f7faf5dcfc63b5022354a0bf9
TK <leandrotk100@gmail.com>
2023-11-27 21:09:48 -0300

    *

---
+++ coding_interviews/algoexpert/validate-subsequence/validate-subsequence-two-pointers.js
@@ -0,0 +1,18 @@
+// Runtime: O(n)
+// Space: O(1)
+
+function isValidSubsequence(array, sequence) {
+  let p1 = 0;
+  let p2 = 0;
+
+  while (p1 < array.length && p2 < sequence.length) {
+    if (array[p1] === sequence[p2]) {
+      p1++;
+      p2++;
+    } else {
+      p1++;
+    }
+  }
+
+  return p2 === sequence.length;
+}
