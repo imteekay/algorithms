1b8e9aa6746afb476f2c01909cba5d6d0cdaf8b8
TK <leandrotk100@gmail.com>
2023-12-01 07:21:34 -0300

    *

---
+++ coding_interviews/algoexpert/tournament-winner/tournament-winner-optimized.js
@@ -0,0 +1,21 @@
+function tournamentWinner(competitions, results) {
+  let teams = new Map();
+  let maxPoints = 0;
+  let winner;
+
+  for (let index = 0; index < competitions.length; index++) {
+    let competition = competitions[index];
+    let result = results[index];
+    let team = result === 1 ? competition[0] : competition[1];
+    let points = (teams.get(team) || 0) + 3;
+
+    teams.set(team, points);
+
+    if (points > maxPoints) {
+      winner = team;
+      maxPoints = points;
+    }
+  }
+
+  return winner;
+}
