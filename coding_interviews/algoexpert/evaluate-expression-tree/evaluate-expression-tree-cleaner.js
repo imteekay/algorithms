59b9dd9694fde86d5f7980e8d9126ce0bead7d2c
TK <leandrotk100@gmail.com>
2023-12-09 09:39:38 -0300

    *

--- coding_interviews/algoexpert/evaluate-expression-tree/evaluate-expression-tree-cleaner.js
+++ coding_interviews/algoexpert/evaluate-expression-tree/evaluate-expression-tree-cleaner.js
@@ -1,13 +1,7 @@
-function evaluateExpressionTree({ value, left, right }) {
-  if (value === -1)
-    return evaluateExpressionTree(left) + evaluateExpressionTree(right);
-  if (value === -2)
-    return evaluateExpressionTree(left) - evaluateExpressionTree(right);
-  if (value === -3)
-    return Math.trunc(
-      evaluateExpressionTree(left) / evaluateExpressionTree(right)
-    );
-  if (value === -4)
-    return evaluateExpressionTree(left) * evaluateExpressionTree(right);
+function evaluateExpressionTree({value, left, right}) {
+  if (value === -1) return evaluateExpressionTree(left) + evaluateExpressionTree(right);
+  if (value === -2) return evaluateExpressionTree(left) - evaluateExpressionTree(right);
+  if (value === -3) return Math.trunc(evaluateExpressionTree(left) / evaluateExpressionTree(right));
+  if (value === -4) return evaluateExpressionTree(left) * evaluateExpressionTree(right);
   return value;
 }
