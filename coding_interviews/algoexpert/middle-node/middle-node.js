57c38090d39ac110d4dbc252343a58960a1f6e7d
TK <leandrotk100@gmail.com>
2023-12-14 08:53:48 -0300

    *

---
+++ coding_interviews/algoexpert/middle-node/middle-node.js
@@ -0,0 +1,21 @@
+function middleNode(linkedList) {
+  let length = 0;
+  let node = linkedList;
+
+  while (linkedList) {
+    linkedList = linkedList.next;
+    length++;
+  }
+
+  let middle = Math.floor(length / 2);
+  let index = 0;
+
+  while (node) {
+    if (index === middle) {
+      return node;
+    }
+
+    node = node.next;
+    index++;
+  }
+}
