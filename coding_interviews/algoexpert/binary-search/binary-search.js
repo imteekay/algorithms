8b6c55307effccb832e7a3cde02a290618205ba6
TK <leandrotk100@gmail.com>
2023-12-19 07:33:56 -0300

    *

---
+++ coding_interviews/algoexpert/binary-search/binary-search.js
@@ -0,0 +1,25 @@
+function binarySearch(array, target) {
+  let start = 0;
+  let end = array.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (start <= end) {
+    let num = array[middle];
+
+    if (num === target) {
+      return middle;
+    }
+
+    if (num > target) {
+      end = middle - 1;
+    }
+
+    if (num < target) {
+      start = middle + 1;
+    }
+
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return -1;
+}
