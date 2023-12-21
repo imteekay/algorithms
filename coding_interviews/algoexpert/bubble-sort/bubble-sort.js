754838390559ba2df13bfa4b28ae40f40524d325
TK <leandrotk100@gmail.com>
2023-12-21 08:03:45 -0300

    *

---
+++ coding_interviews/algoexpert/bubble-sort/bubble-sort.js
@@ -0,0 +1,17 @@
+function bubbleSort(array) {
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+    for (let index = 0; index < array.length - 1; index++) {
+      if (array[index] > array[index + 1]) {
+        let aux = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = aux;
+        sorted = false;
+      }
+    }
+  }
+
+  return array;
+}
