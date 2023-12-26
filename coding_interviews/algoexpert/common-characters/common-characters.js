91db97bacf374a8d8f33201a719cd9f54f993ef9
TK <leandrotk100@gmail.com>
2023-12-26 09:45:45 -0300

    *

--- coding_interviews/algoexpert/common-characters/common-characters.js
+++ coding_interviews/algoexpert/common-characters/common-characters.js
@@ -1,6 +1,19 @@
 // Runtime: (N * M), N = number of strings and M = length of biggest string
 // Space: (M), M = length of biggest string
 
+/**
+ * Write a function that takes in a non-empty list of non-empty strings and returns
+ * a list of characters that are common to all strings in the list, ignoring multiplicity.
+ * Note that the strings are not guaranteed to only contain alphanumeric characters.
+ * The list you return can be in any order.
+ *
+ * Input:
+ * strings = ["abc", "bcd", "cbaccd"]
+ *
+ * Output:
+ * ["b", "c"] // The characters could be ordered differently.
+ */
+
 function commonCharacters(strings) {
   let map = new Map();
 
