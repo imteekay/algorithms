63e77cd104f3f6bd08abd394a9014695b9605d31
TK <leandrotk100@gmail.com>
2023-12-26 09:41:01 -0300

    *

---
+++ coding_interviews/algoexpert/common-characters/common-characters.js
@@ -0,0 +1,21 @@
+function commonCharacters(strings) {
+  let map = new Map();
+
+  for (let string of strings) {
+    let set = new Set(string);
+
+    for (let char of set) {
+      map.set(char, map.get(char) + 1 || 1);
+    }
+  }
+
+  let result = [];
+
+  for (let [char, count] of map) {
+    if (count === strings.length) {
+      result.push(char);
+    }
+  }
+
+  return result;
+}
