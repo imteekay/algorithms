81d35a3847b1b400ffae0dc3d947eef1d9d72f58
TK <leandrotk100@gmail.com>
2023-12-30 10:40:09 -0300

    *

--- coding_interviews/algoexpert/semordnilap/semordnilap.js
+++ coding_interviews/algoexpert/semordnilap/semordnilap.js
@@ -1,3 +1,16 @@
+/**
+Write a function that takes in a list of unique strings
+and returns a list of semordnilap pairs.
+
+A semordnilap pair is defined as a set of different strings
+where the reverse of one word is the same as the forward version
+of the other. For example the words "diaper" and "repaid" are a
+semordnilap pair, as are the words "palindromes" and "semordnilap".
+
+The order of the returned pairs and the order of the strings
+within each pair does not matter.
+*/
+
 // Runtime: O(N * M), N = number of words, M = length o largest word
 // Space: O(N)
 
