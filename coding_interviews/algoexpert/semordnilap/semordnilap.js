88506afcb88f8b07c741ac01b5d7b3bd64e17610
TK <leandrotk100@gmail.com>
2023-12-30 10:30:00 -0300

    *

---
+++ coding_interviews/algoexpert/semordnilap/semordnilap.js
@@ -0,0 +1,24 @@
+function reverse(word) {
+  let reversedWord = [];
+
+  for (let index = word.length - 1; index >= 0; index--) {
+    reversedWord.push(word[index]);
+  }
+
+  return reversedWord.join('');
+}
+
+function semordnilap(words) {
+  let pairs = [];
+  let map = new Map();
+
+  for (let word of words) {
+    if (map.has(word)) {
+      pairs.push([word, map.get(word)]);
+    } else {
+      map.set(reverse(word), word);
+    }
+  }
+
+  return pairs;
+}
