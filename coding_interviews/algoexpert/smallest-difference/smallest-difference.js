d007f2e0c27c81aa8f8f8190a5e085da1d4c5cc0
TK <leandrotk100@gmail.com>
2024-01-01 12:39:26 -0300

    *

--- coding_interviews/algoexpert/smallest-difference/smallest-difference.js
+++ coding_interviews/algoexpert/smallest-difference/smallest-difference.js
@@ -1,3 +1,22 @@
+/**
+ *
+ * Write a function that takes in two non-empty arrays of integers,
+ * finds the pair of numbers (one from each array) whose absolute
+ * difference is closest to zero, and returns an array containing
+ * these two numbers, with the number from the first array in the first position.
+ * Note that the absolute difference of two integers is the distance
+ * between them on the real number line. For example, the absolute
+ * difference of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
+ * You can assume that there will only be one pair of numbers with the smallest difference.
+ *
+ * Input:
+ * arrayOne = [-1, 5, 10, 20, 28, 3]
+ * arrayTwo = [26, 134, 135, 15, 17]
+ *
+ * Output:
+ * [28, 26]
+ */
+
 // Runtime: O(NlogN + MlogM), where N = length of arrayOne and M = length of arrayTwo
 // Space: O(1)
 
