ab5a0c8313c8fb512a9d3affb162f64162cbbed0
TK <leandrotk100@gmail.com>
2024-01-01 17:03:53 -0300

    *

---
+++ coding_interviews/algoexpert/move-element-to-end/move-element-to-end.js
@@ -0,0 +1,33 @@
+/**
+ * You're given an array of integers and an integer. Write a function that
+ * moves all instances of that integer in the array to the end of the
+ * array and returns the array.
+ * The function should perform this in place (i.e., it should mutate the input array)
+ * and doesn't need to maintain the order of the other integers.
+ *
+ * Input:
+ * array = [2, 1, 2, 2, 2, 3, 4, 2]
+ * toMove = 2
+ *
+ * Output:
+ * [1, 3, 4, 2, 2, 2, 2, 2]
+ */
+
+function moveElementToEnd(array, toMove) {
+  let count = 0;
+  let pointer = 0;
+
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] === toMove) count++;
+    else {
+      array[pointer] = array[index];
+      pointer++;
+    }
+  }
+
+  for (let index = pointer; index < array.length; index++) {
+    array[index] = toMove;
+  }
+
+  return array;
+}
