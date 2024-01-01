e23bf45c7b4e0eb1e89ecccf1b337a3a2f5a1c28
TK <leandrotk100@gmail.com>
2024-01-01 12:34:35 -0300

    *

---
+++ coding_interviews/algoexpert/smallest-difference/smallest-difference.js
@@ -0,0 +1,25 @@
+function smallestDifference(arrayOne, arrayTwo) {
+  let output = [];
+  let indexOne = 0;
+  let indexTwo = 0;
+  let smallestDistance = Infinity;
+
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+
+  while (indexOne < arrayOne.length && indexTwo < arrayTwo.length) {
+    let one = arrayOne[indexOne];
+    let two = arrayTwo[indexTwo];
+    let distance = Math.abs(one - two);
+
+    if (distance < smallestDistance) {
+      smallestDistance = distance;
+      output = [one, two];
+    }
+
+    if (one < two) indexOne++;
+    else indexTwo++;
+  }
+
+  return output;
+}
