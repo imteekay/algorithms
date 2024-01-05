780b2d7edb20787866e9718a09c85ac536e99048
TK <leandrotk100@gmail.com>
2024-01-05 07:05:02 -0300

    *

--- coding_interviews/algoexpert/array-of-products/array-of-products.js
+++ coding_interviews/algoexpert/array-of-products/array-of-products.js
@@ -1,3 +1,16 @@
+/**
+ *
+ * Write a function that takes in a non-empty array of integers and returns
+ * an array of the same length, where each element in the output array is
+ * equal to the product of every other number in the input array.
+ * In other words, the value at output[i] is equal to the product of
+ * every number in the input array other than input[i].
+ * Note that you're expected to solve this problem without using division.
+ *
+ * Input: array = [5, 1, 4, 2]
+ * Output: [8, 40, 10, 20]
+ */
+
 // Runtime: O(N^2), N = length of array
 // Space: O(N)
 
