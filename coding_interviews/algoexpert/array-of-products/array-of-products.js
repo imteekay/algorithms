509959bf79cfca5db7f21c377e51183f9c31f54f
TK <leandrotk100@gmail.com>
2024-01-05 06:59:20 -0300

    *

---
+++ coding_interviews/algoexpert/array-of-products/array-of-products.js
@@ -0,0 +1,23 @@
+function initOutput(length) {
+  let output = [];
+
+  while (length--) {
+    output.push(1);
+  }
+
+  return output;
+}
+
+function arrayOfProducts(array) {
+  let output = initOutput(array.length);
+
+  for (let index = 0; index < array.length; index++) {
+    for (let outputIndex = 0; outputIndex < output.length; outputIndex++) {
+      if (index !== outputIndex) {
+        output[outputIndex] *= array[index];
+      }
+    }
+  }
+
+  return output;
+}
