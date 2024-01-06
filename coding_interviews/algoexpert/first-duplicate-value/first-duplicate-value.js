cc0656e7d11249ebd9d5e4220c731c193aeb1c46
TK <leandrotk100@gmail.com>
2024-01-06 11:05:57 -0300

    *

--- coding_interviews/algoexpert/first-duplicate-value/first-duplicate-value.js
+++ coding_interviews/algoexpert/first-duplicate-value/first-duplicate-value.js
@@ -1,3 +1,17 @@
+/**
+ * Given an array of integers between 1 and n, inclusive, where n is the length of
+ * the array, write a function that returns the first integer that appears more
+ * than once (when the array is read from left to right).
+ * In other words, out of all the integers that might occur more than once in the
+ * input array, your function should return the one whose first duplicate
+ * value has the minimum index.
+ * If no integer appears more than once, your function should return -1.
+ * Note that you're allowed to mutate the input array.
+ *
+ * Input: array = [2, 1, 5, 2, 3, 3, 4]
+ * Output: 2
+ */
+
 // Runtime: O(N), N = array length
 // Space: O(N)
 
