600eb31783ec6c1328faa68af209456720bdb3af
TK <leandrotk100@gmail.com>
2024-01-09 08:04:03 -0300

    *

---
+++ coding_interviews/algoexpert/zero-sum-subarray/zero-sum-subarray.js
@@ -0,0 +1,17 @@
+function zeroSumSubarray(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let sum = nums[i];
+
+    for (let j = i; j < nums.length; j++) {
+      if (i !== j) {
+        sum += nums[j];
+      }
+
+      if (sum === 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
