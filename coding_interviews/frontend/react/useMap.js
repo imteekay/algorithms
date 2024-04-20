c0fa6dcfbed823cdeb0d1612694324c63be292f7
TK <leandrotk100@gmail.com>
2024-04-20 10:46:14 -0300

    *

---
+++ coding_interviews/frontend/react/useMap.js
@@ -0,0 +1,36 @@
+import React, { useCallback } from 'react';
+
+function useMap(initialValue) {
+  const [map, setMap] = useState(new Map(initialValue));
+
+  const set = useCallback((key, value) => {
+    setMap((prevMap) => {
+      const updatedMap = new Map(prevMap);
+      updatedMap.set(key, value);
+      return updatedMap;
+    });
+  }, []);
+
+  const del = useCallback((key) => {
+    setMap((prevMap) => {
+      const updatedMap = new Map(prevMap);
+      updatedMap.delete(key);
+      return updatedMap;
+    });
+  }, []);
+
+  const clear = useCallback(() => {
+    setMap((prevMap) => {
+      const updatedMap = new Map(prevMap);
+      updatedMap.clear();
+      return updatedMap;
+    });
+  }, []);
+
+  return {
+    map,
+    set,
+    delete: del,
+    clear,
+  };
+}
