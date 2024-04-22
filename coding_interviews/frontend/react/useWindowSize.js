4be89d3643136eded8c9620d93313b7c816c7789
TK <leandrotk100@gmail.com>
2024-04-22 08:46:16 -0300

    *

---
+++ coding_interviews/frontend/react/useWindowSize.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function useWindowSize() {
+  const [height, setHeight] = useState(window.innerHeight);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setHeight(window.innerHeight);
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return { width, height };
+}
